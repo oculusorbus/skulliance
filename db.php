d8e806acccc0b3cd59fb1665e0d12a0b2cd26fb5
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-06-01 04:22:22 -0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+include 'credentials/db_credentials.php';
+session_start();
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL ^ E_NOTICE);  
+
+
+if(isset($_SESSION['userData']['discord_id'])){
+	if($_SESSION['userData']['discord_id'] == $discordid_oculusorbus) {
+		//$dbname = $dbbametest;
+	}
+}
+?>
