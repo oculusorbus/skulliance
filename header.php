7b7906074f2a1adc36a9d2fb0436d252ceed8eb6
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-06-01 04:19:39 -0500

    Adjustments

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-  <title>Drop Ship</title>
+  <title>Skulliance</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
---
+++ skulliance.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if(!$_SESSION['logged_in']){
+  header('Location: error.php');
+  exit();
+}
+extract($_SESSION['userData']);
+//print_r($_SESSION['userData']);
+//print_r($_POST);
+//exit();
+
+$avatar_url = "https://cdn.discordapp.com/avatars/$discord_id/$avatar.jpg";
+
+?>
