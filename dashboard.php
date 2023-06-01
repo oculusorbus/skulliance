ba2f4455f6056fcd596aca8668a4d30ab03f6b62
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-06-01 04:15:24 -0500

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,1 +1,26 @@
-Hello World
+<?php
+include 'db.php';
+include 'dropship.php';
+include 'webhooks.php';
+include 'header.php';
+?>
+
+<a name="dashboard" id="dashboard"></a>
+<!-- The flexible grid (content) -->
+<div class="row" id="row1">
+  <div class="main">
+    <div class="content">
+	
+    </div>
+  </div>
+  <div class="side">
+  </div>
+</div>
+
+	<!-- Footer -->
+	<div class="footer">
+	  <p>Skulliance<br>Copyright © <span id="year"></span>
+	</div>
+</div>
+</div>
+</body>
