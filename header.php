6dadb615722da5087727bdeedba168c36886c6f5
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-06-01 04:17:07 -0500

    Update header.php

--- header.php
+++ header.php
@@ -7,33 +7,8 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
   <!--<link href="dist/output.css" rel="stylesheet">-->
   <link href="dist/flexbox.css?var=<?php echo rand(0,999); ?>" rel="stylesheet">
-  <?php
-  if($_SESSION["userData"]["project_id"] == 4){?>
-	<?php if(str_contains($_SERVER['PHP_SELF'], "battles.php")){?>
-	<style>
-		.button{
-			filter: hue-rotate(145deg);
-		}
-	</style>
-	  <?php }else{ ?>
-		<style>
-			.button, .small-button{
-				filter: hue-rotate(145deg);
-			}
-			.credit{
-				filter: hue-rotate(45deg);
-			}
-			.battle-credit{
-				filter: hue-rotate(145deg);
-			}
-		</style>
-	  <?php } ?>
-  <?php } ?>
 </head>
 <body>
-	<div id="loading" <?php echo (isset($_POST['run']) || isset($_POST['instant_replay']))?'style="display:none"':""; ?>>
-	  <img id="loading-image" src="<?php echo $prefix;?>images/loading.gif" alt="Loading..." />
-	</div>
 	<div class="container">
 		<!-- Navigation Bar -->
 		<div class="navbar">
