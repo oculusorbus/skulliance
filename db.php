9b64bf8c26462675d7e73569d309c290a27f2c18
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-06-04 03:22:40 -0500

    Update db.php

--- db.php
+++ db.php
@@ -12,4 +12,48 @@ if(isset($_SESSION['userData']['discord_id'])){
 		//$dbname = $dbbametest;
 	}
 }
+
+// Check if user already exists, if not... create them.
+function checkUser($conn) {
+	if(isset($_SESSION['userData']['discord_id'])){
+		$sql = "SELECT id, discord_id, username FROM users WHERE discord_id='".$_SESSION['userData']['discord_id']."'";
+		$result = $conn->query($sql);
+
+		if ($result->num_rows > 0) {
+		  // output data of each row
+		  while($row = $result->fetch_assoc()) {
+		    //echo "id: " . $row["id"]. " - Discord ID: " . $row["discord_id"]. " Username: " . $row["username"]. "<br>";
+	    	$_SESSION['userData']['user_id'] = strval($row["id"]);
+			updateUser($conn);
+		  }
+		} else {
+		  //echo "0 results";
+		  createUser ($conn, $_SESSION);
+		}
+	}
+}
+
+// Create a user that has visited the site for the first time.
+function createUser($conn) {
+	$sql = "INSERT INTO users (discord_id, avatar, username)
+	VALUES ('".$_SESSION['userData']['discord_id']."', '".$_SESSION['userData']['avatar']."', '".$_SESSION['userData']['name']."')";
+
+	if ($conn->query($sql) === TRUE) {
+	  //echo "New record created successfully";
+	} else {
+	  //echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+	// Immediately check user to set session variable and prevent first run errors
+	checkUser($conn);
+}
+
+// Update user to maintain current username
+function updateUser($conn) {
+	$sql = "UPDATE users SET username='".$_SESSION['userData']['name']."', avatar='".$_SESSION['userData']['avatar']."' WHERE id='".$_SESSION['userData']['user_id']."'";
+	if ($conn->query($sql) === TRUE) {
+	  //echo "New record created successfully";
+	} else {
+	  //echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+}
 ?>
