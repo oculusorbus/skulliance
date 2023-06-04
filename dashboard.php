4bf8fe05269b3c0b0cabe9eded471044c6ad6234
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-06-04 05:33:39 -0500

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -15,7 +15,62 @@ $policies = array();
 $policies = getPolicies($conn);
 
 foreach($addresses AS $index => $address){
-	
+	foreach($policies AS $index => $policy){
+		$ch = curl_init("https://api.koios.rest/api/v0/account_assets");
+		curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+		curl_setopt( $ch, CURLOPT_POST, 1);
+		curl_setopt( $ch, CURLOPT_POSTFIELDS, '{"_stake_addresses":["'.$address.'"]}');
+		curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt( $ch, CURLOPT_HEADER, 0);
+		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+
+		$response = curl_exec( $ch );
+		// If you need to debug, or find out why you can't send message uncomment line below, and execute script.
+		$response = json_decode($response);
+		//print_r($response[0]->asset_list);
+		//exit;
+		curl_close( $ch );
+
+		//$_SESSION['userData']['nfts'] = array();
+	    if(isset($response[0])){
+			$asset_names = array();
+			$counter = 0;
+			foreach($response[0]->asset_list AS $index => $token){
+				if($token->policy_id == $policy){
+					$tokench = curl_init("https://api.koios.rest/api/v0/asset_info?_asset_policy=".$token->policy_id."&_asset_name=".$token->asset_name);
+					curl_setopt( $tokench, CURLOPT_RETURNTRANSFER, 1);
+					$tokenresponse = curl_exec( $tokench );
+					$tokenresponse = json_decode($tokenresponse);
+					curl_close( $tokench );
+					if(isset($tokenresponse[0])){
+						foreach($tokenresponse[0]->minting_tx_metadata AS $metadata){
+							$counter++;
+							$policy_id = $token->policy_id;
+							$asset_name = $tokenresponse[0]->asset_name_ascii;
+							$nft = $metadata->$policy_id;
+							$nft_data = $nft->$asset_name;
+							$ipfs = substr($nft_data->image, 7, strlen($nft_data->image));
+							// Account for NFT with NaN value for asset name
+							if($asset_name == "NaN"){
+								$nft_data->AssetName = "DROPSHIP012";
+							}else{
+								$nft_data->AssetName = $asset_name;
+							}
+							//renderNFT($nft_data, $ipfs);
+							/* Removing to rely on database now
+							if($_SESSION['userData']['project_id'] != 1){
+								$_SESSION['userData']['nfts'][] = $nft_data;
+							}*/
+							$asset_names[] = $nft_data->AssetName;
+							print_r($nft_data);
+							//createNFT($conn, $nft_data->AssetName, $nft_data->name, $ipfs);
+						} // End foreach
+					}// End if
+				} // End if
+			} // End foreach
+			//updateNFTs($conn, implode("', '", $asset_names));
+		} // End if
+	}
 }
 ?>
 
