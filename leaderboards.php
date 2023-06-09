2a3d00c47743e1c03089a5e8dedccec85c52660c
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-06-09 01:21:39 -0500

    Update leaderboards.php

--- leaderboards.php
+++ leaderboards.php
@@ -23,6 +23,60 @@ include 'header.php';
 				</div>
 			</div>
 		</div>
+		<div class="row" id="row2">
+			<div class="col1of2">
+			    <div class="content">
+				    <?php
+						echo "<h2>Galactico</h2>";
+						checkLeaderboard($conn, false, 1);
+					?>
+				</div>
+			</div>
+			<div class="col1of2">
+				<div class="content">
+				    <?php
+						echo "<h2>Ohh Meed</h2>";
+						checkLeaderboard($conn, false, 2);
+					?>
+				</div>
+			</div>
+		</div>
+		<div class="row" id="row2">
+			<div class="col1of2">
+			    <div class="content">
+				    <?php
+						echo "<h2>H.Y.P.E.</h2>";
+						checkLeaderboard($conn, false, 3);
+					?>
+				</div>
+			</div>
+			<div class="col1of2">
+				<div class="content">
+				    <?php
+						echo "<h2>Sinder Skullz</h2>";
+						checkLeaderboard($conn, false, 4);
+					?>
+				</div>
+			</div>
+		</div>
+		<div class="row" id="row2">
+			<div class="col1of2">
+			    <div class="content">
+				    <?php
+						echo "<h2>Kimosabe Art</h2>";
+						checkLeaderboard($conn, false, 5);
+					?>
+				</div>
+			</div>
+			<div class="col1of2">
+				<div class="content">
+				    <?php
+						echo "<h2>Crypties</h2>";
+						checkLeaderboard($conn, false, 6);
+					?>
+				</div>
+			</div>
+		</div>
 		<!-- Footer -->
 		<div class="footer">
 		  <p>Skulliance<br>Copyright © <span id="year"></span>
