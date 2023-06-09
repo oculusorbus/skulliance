3b151b29de7acfb670587780e7f73d6119b3beb4
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-06-09 00:27:13 -0500

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -67,7 +67,7 @@ if(isset($_POST['item_id'])) {
 			alert("You cannot purchase this item because it is out of stock.");
 		}
 	}else{
-		alert("You/'ve already purchased this item before. Don/'t be greedy.");
+		alert("You\'ve already purchased this item before. Don\'t be greedy.");
 	}
 }
 
