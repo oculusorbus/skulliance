4582dfda36138d395aff93e88cc1aa3f6e0b8e79
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-06-09 01:14:36 -0500

    Update leaderboards.php

--- leaderboards.php
+++ leaderboards.php
@@ -5,12 +5,20 @@ include 'skulliance.php';
 include 'header.php';
 ?>
 		<a name="leaderboards" id="leaderboards"></a>
-		<div class="row" id="row3">
-			<div class="col1of3">
+		<div class="row" id="row2">
+			<div class="col1of2">
 			    <div class="content">
 				    <?php
 						echo "<h2>Skulliance</h2>";
 						checkLeaderboard($conn, false, 7);
+					?>
+				</div>
+			</div>
+			<div class="col1of2">
+				<div class="content">
+				    <?php
+						echo "<h2>Galactico</h2>";
+						checkLeaderboard($conn, false, 1);
 					?>
 				</div>
 			</div>
