001517dc5ac2cbca1defacb8aa0bcefff6838dc0
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-06-09 02:04:02 -0500

    Update db.php

--- db.php
+++ db.php
@@ -664,7 +664,7 @@ function checkLeaderboard($conn, $clean, $project_id=0) {
 	if($project_id != 0){
 		$where = "WHERE collections.project_id = '".$project_id."'";
 	}
-	$sql = "SELECT nfts.id, nfts.user_id, COUNT(nfts.id) as total, users.username, users.discord_id AS discord_id, avatar, projects.id AS project_id FROM nfts INNER JOIN users ON nfts.user_id=users.id INNER JOIN collections ON collections.id = nfts.collection_id INNER JOIN projects ON projects.id = collections.project_id ".$where." GROUP BY nfts.user_id ORDER BY total DESC LIMIT 10";
+	$sql = "SELECT nfts.id, nfts.user_id, COUNT(nfts.id) as total, users.username, users.discord_id AS discord_id, avatar, projects.id AS project_id FROM nfts INNER JOIN users ON nfts.user_id=users.id INNER JOIN collections ON collections.id = nfts.collection_id INNER JOIN projects ON projects.id = collections.project_id ".$where." GROUP BY nfts.user_id ORDER BY total DESC";
 	$result = $conn->query($sql);
 
 	if ($result->num_rows > 0) {
