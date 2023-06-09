bbae09350001d02e2b493b6641be9bbce794ae0b
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-06-08 23:36:13 -0500

    Update rewards.php

--- rewards.php
+++ rewards.php
@@ -1,7 +1,9 @@
 <?php
 include_once 'db.php';
 
+if(isset($argv)){
 parse_str(implode('&', array_slice($argv, 1)), $_GET);
+}
 if(isset($_GET['deploy'])){
 	set_time_limit(0);
 	updateBalances($conn);
