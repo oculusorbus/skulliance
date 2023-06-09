4c986ee7f884ea3808721c88663c42a18a7f1790
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-06-09 01:24:15 -0500

    Update header.php

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 	      <img class="rounded-full" src="<?php echo $avatar_url?>" />
 		  <a href="https://discord.gg/DHbGU9ZDyG"><?php echo $name;?></a>
 		  <a class="navbar-first" href="dashboard.php">Dashboard</a>
+		  <a href="leaderboards.php">Leaderboards</a>
 		  <a href="transactions.php">Transactions</a>
 		  <a href="logout.php">Logout</a>
 		</div>
