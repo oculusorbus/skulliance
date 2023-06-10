d8799b48f2512830b03c5d152bb764d53e5ba26d
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-06-10 01:05:40 -0500

    Update verify.php

--- verify.php
+++ verify.php
@@ -74,30 +74,32 @@ function verifyNFTs($conn, $addresses, $policies){
 						$asset_name = $tokenresponse[0]->asset_name_ascii;
 						if(isset($metadata->$policy_id)){
 							$nft = $metadata->$policy_id;
-							$nft_data = $nft->$asset_name;
-							if(isset($nft_data->image)){
-								$ipfs = substr($nft_data->image, 7, strlen($nft_data->image));
-							}else{
-								$ipfs = "";
-							}
-							// Account for NFT with NaN value for asset name
-							if($asset_name == "NaN"){
-								$nft_data->AssetName = "DROPSHIP012";
-							}else{
-								$nft_data->AssetName = $asset_name;
-							}
-							$asset_names[] = $nft_data->AssetName;
-							if(isset($_SESSION['userData']['user_id'])){
-								$user_id = $_SESSION['userData']['user_id'];
-							}else{
-								$user_id = getUserId($conn, $address);
-							}
-							if(isset($nft_data->name)){
-								if(checkNFT($conn, $token->fingerprint)){
-									updateNFT($conn, $token->fingerprint, $user_id);
+							if(isset($nft->$asset_name)){
+								$nft_data = $nft->$asset_name;
+								if(isset($nft_data->image)){
+									$ipfs = substr($nft_data->image, 7, strlen($nft_data->image));
 								}else{
-									$collection_id = getCollectionId($conn, $policy_id);
-									createNFT($conn, $token->fingerprint, $nft_data->AssetName, $nft_data->name, $ipfs, $collection_id, $user_id);
+									$ipfs = "";
+								}
+								// Account for NFT with NaN value for asset name
+								if($asset_name == "NaN"){
+									$nft_data->AssetName = "DROPSHIP012";
+								}else{
+									$nft_data->AssetName = $asset_name;
+								}
+								$asset_names[] = $nft_data->AssetName;
+								if(isset($_SESSION['userData']['user_id'])){
+									$user_id = $_SESSION['userData']['user_id'];
+								}else{
+									$user_id = getUserId($conn, $address);
+								}
+								if(isset($nft_data->name)){
+									if(checkNFT($conn, $token->fingerprint)){
+										updateNFT($conn, $token->fingerprint, $user_id);
+									}else{
+										$collection_id = getCollectionId($conn, $policy_id);
+										createNFT($conn, $token->fingerprint, $nft_data->AssetName, $nft_data->name, $ipfs, $collection_id, $user_id);
+									}
 								}
 							}
 						}
