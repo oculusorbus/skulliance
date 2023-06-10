b4bc66cfcf71160560f2874f082f38dbeaba529b
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-06-10 00:49:46 -0500

    Update verify.php

--- verify.php
+++ verify.php
@@ -53,7 +53,6 @@ function verifyNFTs($conn, $addresses, $policies){
 				} // End if
 			} // End foreach
 			print_r($asset_list);
-			exit;
 			/*
 			$tokench = curl_init("https://api.koios.rest/api/v0/asset_info");
 			curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
