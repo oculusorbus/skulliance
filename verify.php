a61d0962a2da8b47da0a8a4604f8f1c18e4787cb
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-06-10 00:46:08 -0500

    Verify Test

--- verify.php
+++ verify.php
@@ -41,53 +41,69 @@ function verifyNFTs($conn, $addresses, $policies){
 	    if(isset($response[0])){
 			$asset_names = array();
 			$counter = 0;
+			$asset_list = array();
+			$asset_list["_asset_list"] = array();
 			foreach($response[0]->asset_list AS $index => $token){
 				if(in_array($token->policy_id, $policies)){
-					$tokench = curl_init("https://api.koios.rest/api/v0/asset_info?_asset_policy=".$token->policy_id."&_asset_name=".$token->asset_name);
-					curl_setopt( $tokench, CURLOPT_RETURNTRANSFER, 1);
-					$tokenresponse = curl_exec( $tokench );
-					$tokenresponse = json_decode($tokenresponse);
-					curl_close( $tokench );
-					if(is_array($tokenresponse)){
-						foreach($tokenresponse[0]->minting_tx_metadata AS $metadata){
-							$counter++;
-							$policy_id = $token->policy_id;
-							if(isset($tokenresponse[0]->asset_name_ascii)){
-								$asset_name = $tokenresponse[0]->asset_name_ascii;
-								if(isset($metadata->$policy_id)){
-									$nft = $metadata->$policy_id;
-									$nft_data = $nft->$asset_name;
-									if(isset($nft_data->image)){
-										$ipfs = substr($nft_data->image, 7, strlen($nft_data->image));
-									}else{
-										$ipfs = "";
-									}
-									// Account for NFT with NaN value for asset name
-									if($asset_name == "NaN"){
-										$nft_data->AssetName = "DROPSHIP012";
-									}else{
-										$nft_data->AssetName = $asset_name;
-									}
-									$asset_names[] = $nft_data->AssetName;
-									if(isset($_SESSION['userData']['user_id'])){
-										$user_id = $_SESSION['userData']['user_id'];
-									}else{
-										$user_id = getUserId($conn, $address);
-									}
-									if(isset($nft_data->name)){
-										if(checkNFT($conn, $token->fingerprint)){
-											updateNFT($conn, $token->fingerprint, $user_id);
-										}else{
-											$collection_id = getCollectionId($conn, $policy_id);
-											createNFT($conn, $token->fingerprint, $nft_data->AssetName, $nft_data->name, $ipfs, $collection_id, $user_id);
-										}
-									}
-								}
-							}
-						} // End foreach
-					}// End if
+					$asset_list["_asset_list"][$counter] = array();
+					$asset_list["_asset_list"][$counter][0] = $token->policy_id;
+					$asset_list["_asset_list"][$counter][1] = $token->asset_name;
+					$counter++;
+					
 				} // End if
 			} // End foreach
+			print_r($asset_list);
+			exit;
+			$tokench = curl_init("https://api.koios.rest/api/v0/asset_info");
+			curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+			curl_setopt( $ch, CURLOPT_POST, 1);
+			curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode($asset_list));
+			curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt( $ch, CURLOPT_HEADER, 0);
+			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+			
+			//$tokench = curl_init("https://api.koios.rest/api/v0/asset_info?_asset_policy=".$token->policy_id."&_asset_name=".$token->asset_name);
+			curl_setopt( $tokench, CURLOPT_RETURNTRANSFER, 1);
+			$tokenresponse = curl_exec( $tokench );
+			$tokenresponse = json_decode($tokenresponse);
+			curl_close( $tokench );
+			if(is_array($tokenresponse)){
+				foreach($tokenresponse[0]->minting_tx_metadata AS $metadata){
+					$policy_id = $token->policy_id;
+					if(isset($tokenresponse[0]->asset_name_ascii)){
+						$asset_name = $tokenresponse[0]->asset_name_ascii;
+						if(isset($metadata->$policy_id)){
+							$nft = $metadata->$policy_id;
+							$nft_data = $nft->$asset_name;
+							if(isset($nft_data->image)){
+								$ipfs = substr($nft_data->image, 7, strlen($nft_data->image));
+							}else{
+								$ipfs = "";
+							}
+							// Account for NFT with NaN value for asset name
+							if($asset_name == "NaN"){
+								$nft_data->AssetName = "DROPSHIP012";
+							}else{
+								$nft_data->AssetName = $asset_name;
+							}
+							$asset_names[] = $nft_data->AssetName;
+							if(isset($_SESSION['userData']['user_id'])){
+								$user_id = $_SESSION['userData']['user_id'];
+							}else{
+								$user_id = getUserId($conn, $address);
+							}
+							if(isset($nft_data->name)){
+								if(checkNFT($conn, $token->fingerprint)){
+									updateNFT($conn, $token->fingerprint, $user_id);
+								}else{
+									$collection_id = getCollectionId($conn, $policy_id);
+									createNFT($conn, $token->fingerprint, $nft_data->AssetName, $nft_data->name, $ipfs, $collection_id, $user_id);
+								}
+							}
+						}
+					}
+				} // End foreach
+			}// End if
 			//updateNFTs($conn, implode("', '", $asset_names));
 		} // End if
 		}
