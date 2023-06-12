d344b0a14d1f017c9f4b5afdc7b5b0be6d148a78
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-06-12 12:12:24 -0500

    Update store.php

--- store.php
+++ store.php
@@ -44,8 +44,7 @@ include 'header.php';
 <?php
 // Close DB Connection
 $conn->close();
-if($filterby != ""){
-	echo "<script type='text/javascript'>document.getElementById('filterLeaderboard').value = '".$filterby."';</script>";
-}?>
+?>
+<script type="module" src="wallet.js?var=<?php echo rand(0,999); ?>"></script>
 <script type="text/javascript" src="skulliance.js"></script>
 </html>
