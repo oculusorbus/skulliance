60e0c7bf8d11f9cd59a12ce5867624c12e521d51
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-06-17 16:41:19 -0500

    Update skulliance.php

--- skulliance.php
+++ skulliance.php
@@ -122,7 +122,7 @@ if(isset($_POST['stakeaddress'])){
 	$policies = array();
 	$policies = getPolicies($conn);
 	verifyNFTs($conn, $addresses, $policies);
-	assignRole($_SESSION['userData']['user_id'], "1119732763956871199");
+	assignRole("772831523899965440", "1119732763956871199");
 	alert("Your wallet with stake address: ".$_POST['stakeaddress']." has been successfully connected. The qualifying NFTs in your wallet have now been verified. You can connect additional wallets as well. They will not replace the wallet you just connected. You have also been assigned the Staker role in the Skulliance discord. Enjoy Skulliance staking!");
 }
 
