aa0d8202dd5393c06c408cf92d2b705e591fced6
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-06-26 01:39:36 -0500

    Update db.php

--- db.php
+++ db.php
@@ -525,7 +525,7 @@ function getBalance($conn, $project_id){
 
 // Render JS alert message
 function alert($message){
-	echo "<script type='text/javascript'>document.body.onload = function(){alert('".$message."');};</script>";
+	echo "<script type='text/javascript'>$( document ).ready(function() {alert('".$message."');});</script>";
 }
 
 // Zero out all currency upon user creation
