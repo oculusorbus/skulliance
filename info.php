58295e9a0052e9e36e5ff42fd4d06ab70a0b20e7
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-07-31 19:20:02 -0500

    Update info.php

--- info.php
+++ info.php
@@ -25,7 +25,7 @@ include 'header.php';
 					Once the necessary NFTs are verified in Discord and you've claimed your membership, you will be able to stake with Skulliance, earn off-chain currency, and claim NFT/FT rewards.
 				</p>
 				<video width="100%" controls>
-				  <source src="staking.mp4" type="video/mp4">
+				  <source src="images/staking.mp4" type="video/mp4">
 				Your browser does not support the video tag.
 				</video>
 			</div>
