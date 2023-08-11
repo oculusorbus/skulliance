82ac71a3322db383f1d44f99f0be3c494277c5fb
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-08-11 15:42:48 -0500

    Update image.php

--- image.php
+++ image.php
@@ -3,8 +3,17 @@
 $filename = 'https://ipfs.io/ipfs/'.$_GET['ipfs'];
 $percent = 0.25; // percentage of resize
 
+// Get image filetype
+$filetype = exif_imagetype($filename);
+
 // Content type
-header('Content-type: image/jpeg');
+if($filetype == "IMAGETYPE_JPEG"){
+	header('Content-type: image/jpeg');
+}else if($filetype == "IMAGETYPE_GIF"){
+	header('Content-type: image/gif');
+}else if($filetype == "IMAGETYPE_PNG"){
+	header('Content-type: image/png');
+}
 
 // Get new dimensions
 list($width, $height) = getimagesize($filename);
@@ -13,7 +22,14 @@ $new_height = $height * $percent;
 
 // Resample
 $image_p = imagecreatetruecolor($new_width, $new_height);
-$image = imagecreatefromgif($filename);
+if($filetype == "IMAGETYPE_JPEG"){
+	$image = imagecreatefromjpeg($filename);
+}else if($filetype == "IMAGETYPE_GIF"){
+	$image = imagecreatefromgif($filename);
+}else if($filetype == "IMAGETYPE_PNG"){
+	$image = imagecreatefrompng($filename);
+}
+
 imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
 
 // Output
