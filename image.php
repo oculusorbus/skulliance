1c16f574c3580f5f5e4083564d7d8b97bad33a2d
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-08-11 15:38:20 -0500

    Update image.php

--- image.php
+++ image.php
@@ -13,7 +13,7 @@ $new_height = $height * $percent;
 
 // Resample
 $image_p = imagecreatetruecolor($new_width, $new_height);
-$image = imagecreatefromjpeg($filename);
+$image = imagecreatefromgif($filename);
 imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
 
 // Output
