2da73126c75454696aff62f0c8fe02952bbc8014
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-08-22 14:08:57 -0500

    Update error.php

--- error.php
+++ error.php
@@ -4,6 +4,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="dist/output.css" rel="stylesheet">
+  <style>
+	  .text-3xl, .mt-3 {
+	  	  background-color: black;
+		  padding: 10px;
+	  }
+  </style>
 </head>
 <body>
     <div class="flex items-center justify-center h-screen bg-discord-gray flex-col">
