af109fbc809931f1a9934896fe613372b928a3c2
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-08-22 14:10:10 -0500

    Update error.php

--- error.php
+++ error.php
@@ -5,8 +5,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="dist/output.css" rel="stylesheet">
   <style>
-	  .text-3xl, .mt-3 {
+	  .text-3xl {
 	  	  background-color: black;
+		  padding: 20px;
+	  }
+	  .mt-3 {
+	  	  background-color: purple;
 		  padding: 20px;
 	  }
   </style>
