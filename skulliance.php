47a9f38476713f266c50764d822e4b75a7366a1e
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-09-04 17:56:25 -0500

    Update skulliance.php

--- skulliance.php
+++ skulliance.php
@@ -207,7 +207,7 @@ function renderCrafting($conn, $page){
 		</li>
 		<li class="role">
 		<form onsubmit="return confirm('Do you really want to shatter this $DIAMOND?');" id="diamondForm" action="<?php echo $page; ?>.php" method="post">
-		  <br>Shatter the following amount of $DIAMOND to equal parts core project currency:<br><br>
+		  <br>Shatter the following amount of $DIAMOND to equal parts core currency:<br><br>
 		  <img class="icon" src="icons/diamond.png">MAX&nbsp;
 		  <input type="number" size="10" id="diamond" name="diamond" min="1" max="<?php echo $diamond;?>" value="<?php echo $diamond;?>">	
 		  <input type="submit" value="Shatter" class="small-button">
