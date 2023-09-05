4a0dc7e7cc74dbcd58d89b214fb63acf5cd3c1c6
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-09-05 12:52:40 -0500

    Update skulliance.php

--- skulliance.php
+++ skulliance.php
@@ -16,15 +16,16 @@ $avatar_url = "https://cdn.discordapp.com/avatars/$discord_id/$avatar.jpg";
 
 // Initiate variables
 $member = false;
-print_r($_SESSION);
 $roles = $_SESSION['userData']['roles'];
-foreach ($roles as $key => $roleData) {
-	switch ($roleData) {
-	  case "949930195584954378":
-		$member = true;
-		break;
-	  default:
-		break;
+if(!empty($roles)){
+	foreach ($roles as $key => $roleData) {
+		switch ($roleData) {
+		  case "949930195584954378":
+			$member = true;
+			break;
+		  default:
+			break;
+		}
 	}
 }
 /*
