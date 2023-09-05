f65521d5ffee5a40f314bf0de641b94c1b88b099
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-09-05 12:20:38 -0500

    Update store.php

--- store.php
+++ store.php
@@ -47,7 +47,7 @@ include 'header.php';
 					<div id="nfts" class="nfts">
 						<?php 
 						if(!$member){
-							echo "<p>You must become a member of Skulliance before you can claim items from the store.<br><a href='info.php'>View info on how to become a member of Skulliance.</a></p>";
+							echo "<p>You must become a member of Skulliance before you can claim items from the store.<br><br><a href='info.php'>View info on how to become a member of Skulliance.</a></p>";
 						}
 						getItems($conn, "store", $filterby);
 						?>				
