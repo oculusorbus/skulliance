00e6bd8e2b112c64f588a50cd647fc174ed64ec9
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-09-05 11:34:27 -0500

    Update wallets.php

--- wallets.php
+++ wallets.php
@@ -17,26 +17,28 @@ if(isset($_POST['wallet_id'])){
 					<div class="content" id="transactions-pane">
 						<?php renderWalletConnection("wallets"); ?>
 						<?php
-						$wallets = getWallets($conn);
-						$wallet_counter = 1;
-						foreach($wallets AS $id => $wallet){ 
-							?>
-							<li class="role">
-								<?php
-								echo $wallet_counter.".&nbsp;";
-								if($wallet["main"] == "0"){
-									echo "<form id='walletForm' action='wallets.php' method='post'>";
-									echo "<input type='hidden' id='wallet_id' name='wallet_id' value='".$id."'>";
-									echo "<input type='submit' value='Make Primary' class='small-button'>";
-									echo "</form>";
-								}else{
-									echo "Primary Address: ";
-								}
-								echo "&nbsp;<a href='https://pool.pm/".$wallet["address"]."' target='_blank'>".substr($wallet["address"], -20)."</a>&nbsp;";
+						if(isset($_SESSION['userData']['user_id']){ ?>
+							$wallets = getWallets($conn);
+							$wallet_counter = 1;
+							foreach($wallets AS $id => $wallet){ 
 								?>
-							</li>
-						<?php 
-						$wallet_counter++;
+								<li class="role">
+									<?php
+									echo $wallet_counter.".&nbsp;";
+									if($wallet["main"] == "0"){
+										echo "<form id='walletForm' action='wallets.php' method='post'>";
+										echo "<input type='hidden' id='wallet_id' name='wallet_id' value='".$id."'>";
+										echo "<input type='submit' value='Make Primary' class='small-button'>";
+										echo "</form>";
+									}else{
+										echo "Primary Address: ";
+									}
+									echo "&nbsp;<a href='https://pool.pm/".$wallet["address"]."' target='_blank'>".substr($wallet["address"], -20)."</a>&nbsp;";
+									?>
+								</li>
+							<?php 
+							$wallet_counter++;
+							}
 						} ?>
 						</ul>
 				</div>
