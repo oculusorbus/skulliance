5d1958d6da9f2c8385babef58d95ec3948748f3d
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-09-05 11:56:30 -0500

    Update info.php

--- info.php
+++ info.php
@@ -23,7 +23,7 @@ include 'header.php';
 					<a href="http://discord.gg/JqqBZBrph2" target="_blank">Join Skulliance Discord</a> for a full list of qualifying policies, verification of NFTs, and claiming membership. The minimum total cost for all the required NFTs for membership is approx 30 ADA.
 				</p>
 				<p>
-					Once the necessary NFTs are verified in Discord and you've claimed your membership, you will be able to claim NFT/FT rewards.
+					Once the necessary NFTs are verified in Discord and you've setup your membership, you will be able to claim NFT/FT rewards.
 				</p>
 				<video width="100%" style="background-color:black" controls>
 				  <source src="images/staking.mp4" type="video/mp4">
