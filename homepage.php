c7dde1ae900df900075e9afb84ba58e7e264a551
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-10-05 11:05:54 -0500

    Test Homepage and Testing Blockfrost CIP68 Fallback

---
+++ homepage.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+  <title>Skulliance</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  <!--<link href="dist/output.css" rel="stylesheet">-->
+  <link href="https://www.skulliance.io/staking/dist/flexbox.css?var=<?php echo rand(0,999); ?>" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		<!-- Navigation Bar -->
+		<div class="navbar">
+		  <a class="navbar-first" href="store.php">Store</a>
+		  <a href="leaderboards.php">Leaderboards</a>
+		  <a href="collections.php">Collections</a>
+		  <a href="transactions.php">Transactions</a>
+    	  <a href="wallets.php">Wallets</a>
+		  <a href="../shop">Merch</a>
+		</div>
+
+<!-- The flexible grid (content) -->
+<div class="row" id="row1">
+  <div class="main">
+	
+  </div>
+</div>
+
+	<!-- Footer -->
+	<div class="footer">
+	  <p>Skulliance<br>Copyright © <span id="year"></span>
+	</div>
+</div>
+</body>
+</html>
--- verify.php
+++ verify.php
@@ -68,40 +68,47 @@ function verifyNFTs($conn, $addresses, $policies){
 			curl_close( $tokench );
 			if(is_array($tokenresponse)){
 				foreach($tokenresponse AS $index => $tokenresponsedata){
-				foreach($tokenresponsedata->minting_tx_metadata AS $metadata){
-					$policy_id = $tokenresponsedata->policy_id;
-					if(isset($tokenresponsedata->asset_name_ascii)){
-						$asset_name = $tokenresponsedata->asset_name_ascii;
-						if(isset($metadata->$policy_id)){
-							$nft = $metadata->$policy_id;
-							$nft_data = $nft->$asset_name;
-							if(isset($nft_data->image)){
-								$ipfs = substr($nft_data->image, 7, strlen($nft_data->image));
-							}else{
-								$ipfs = "";
-							}
-							// Account for NFT with NaN value for asset name
-							if($asset_name == "NaN"){
-								$nft_data->AssetName = "DROPSHIP012";
-							}else{
-								$nft_data->AssetName = $asset_name;
-							}
-							if(isset($_SESSION['userData']['user_id'])){
-								$user_id = $_SESSION['userData']['user_id'];
-							}else{
-								$user_id = getUserId($conn, $address);
-							}
-							if(isset($nft_data->name)){
-								if(checkNFT($conn, $tokenresponsedata->fingerprint)){
-									updateNFT($conn, $tokenresponsedata->fingerprint, $user_id);
-								}else{
-									$collection_id = getCollectionId($conn, $policy_id);
-									createNFT($conn, $tokenresponsedata->fingerprint, $nft_data->AssetName, $nft_data->name, $ipfs, $collection_id, $user_id);
+					if(is_array($tokenresponsedata->minting_tx_metadata)){
+						foreach($tokenresponsedata->minting_tx_metadata AS $metadata){
+							$policy_id = $tokenresponsedata->policy_id;
+							if(isset($tokenresponsedata->asset_name_ascii)){
+								$asset_name = $tokenresponsedata->asset_name_ascii;
+								if(isset($metadata->$policy_id)){
+									$nft = $metadata->$policy_id;
+									$nft_data = $nft->$asset_name;
+									if(isset($nft_data->image)){
+										$ipfs = substr($nft_data->image, 7, strlen($nft_data->image));
+									}else{
+										$ipfs = "";
+									}
+									// Account for NFT with NaN value for asset name
+									if($asset_name == "NaN"){
+										$nft_data->AssetName = "DROPSHIP012";
+									}else{
+										$nft_data->AssetName = $asset_name;
+									}
+									if(isset($_SESSION['userData']['user_id'])){
+										$user_id = $_SESSION['userData']['user_id'];
+									}else{
+										$user_id = getUserId($conn, $address);
+									}
+									if(isset($nft_data->name)){
+										if(checkNFT($conn, $tokenresponsedata->fingerprint)){
+											updateNFT($conn, $tokenresponsedata->fingerprint, $user_id);
+										}else{
+											$collection_id = getCollectionId($conn, $policy_id);
+											createNFT($conn, $tokenresponsedata->fingerprint, $nft_data->AssetName, $nft_data->name, $ipfs, $collection_id, $user_id);
+										}
+									}
 								}
 							}
-						}
+						} // End foreach
+					// Empty Koios metadata, Use Blockfrost for CIP68
+					}else{
+						echo "Policy ID: ".$tokenresponsedata->policy_id."<br>";
+						echo "Asset Name: ".$tokenresponsedata->asset_name;
+						exit;
 					}
-				} // End foreach
 				} // End foreach
 			}// End if
 			//updateNFTs($conn, implode("', '", $asset_names));
