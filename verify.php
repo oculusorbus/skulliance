3219fb60e6c3c77d007cbc922c6f383fb9c31909
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-10-05 14:22:36 -0500

    Update verify.php

--- verify.php
+++ verify.php
@@ -163,7 +163,7 @@ function hex2str($hex) {
 }
 
 function clean($string) {
-   $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+   //$string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
 
    return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
 }
