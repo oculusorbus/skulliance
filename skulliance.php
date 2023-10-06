8c92a355dc6547c61dabcce66de76bbe650bf0bf
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-10-05 21:33:06 -0500

    Update skulliance.php

--- skulliance.php
+++ skulliance.php
@@ -201,8 +201,8 @@ function renderCrafting($conn, $page){
 	$balances = array();
 	$balances = getBalances($conn);
 	if(!empty($balances)){
-		$diamond = $balances["\$DIAMOND"];
-		unset($balances["\$DIAMOND"]);
+		$diamond = $balances["DIAMOND"];
+		unset($balances["DIAMOND"]);
 		$zero = false;
 		foreach($balances AS $currency => $balance){
 			if($balance == 0){
@@ -214,7 +214,7 @@ function renderCrafting($conn, $page){
 		}else{
 			?>
 			<form onsubmit="return confirm('Do you really want to convert all currency to DIAMOND?');" id="craftingForm" action="<?php echo $page; ?>.php" method="post">
-			  Convert the following amount of core project currency to $DIAMOND:<br><br>
+			  Convert the following amount of core project currency to DIAMOND:<br><br>
 			  <img class="icon" src="icons/diamond.png">MAX&nbsp;
 			  <input type="number" size="10" id="balance" name="balance" min="1" max="<?php echo min($balances);?>" value="<?php echo min($balances);?>">	
 			  <input type="submit" value="Convert" class="small-button">
@@ -226,7 +226,7 @@ function renderCrafting($conn, $page){
 			</li>
 			<li class="role">
 			<form onsubmit="return confirm('Do you really want to shatter this DIAMOND?');" id="diamondForm" action="<?php echo $page; ?>.php" method="post">
-			  <br>Shatter the following amount of $DIAMOND to equal parts core currency:<br><br>
+			  <br>Shatter the following amount of DIAMOND to equal parts core currency:<br><br>
 			  <img class="icon" src="icons/diamond.png">MAX&nbsp;
 			  <input type="number" size="10" id="diamond" name="diamond" min="1" max="<?php echo $diamond;?>" value="<?php echo $diamond;?>">	
 			  <input type="submit" value="Shatter" class="small-button">
