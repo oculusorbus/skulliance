4cfb3b7ccbdaceb1feaccf63f57b45eedb9ec9b4
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-11-19 21:04:45 -0600

    Create diamond-skulls.php

---
+++ diamond-skulls.php
@@ -0,0 +1,47 @@
+<?php
+include_once 'db.php';
+include 'webhooks.php';
+include 'message.php';
+include 'role.php';
+include 'verify.php';
+include 'skulliance.php';
+include 'header.php';
+?>
+
+<a name="diamond-skulls" id="diamond-skulls"></a>
+<!-- The flexible grid (content) -->
+<div class="row" id="row1">
+  <div class="main">
+	<h2>Diamond Skulls</h2>
+	<a name="holdings" id="holdings"></a>
+    <div class="content">
+		<?php filterNFTs("diamond-skulls"); ?>
+		<div id="nfts" class="nfts">
+			<?php 
+			if(isset($_SESSION['userData']['user_id'])){ 
+				getNFTs($conn, $filterby=7); 
+			}else{
+				echo "<p>Please connect a Cardano wallet to view your qualifying NFTs.<br><br>Once you begin staking your NFTs, you will need to become a Skulliance member before you can claim items from the store.<br><br><a href='info.php'>View info on how to become a member of Skulliance.</a></p>";
+			} 
+			?>
+		</div>
+    </div>
+  </div>
+</div>
+
+	<!-- Footer -->
+	<div class="footer">
+	  <p>Skulliance<br>Copyright © <span id="year"></span>
+	</div>
+</div>
+</div>
+</body>
+<?php
+// Close DB Connection
+$conn->close();
+if($filterby != ""){
+	echo "<script type='text/javascript'>document.getElementById('filterNFTs').value = '".$filterby."';</script>";
+}?>
+<script type="module" src="wallet.js?var=<?php echo rand(0,999); ?>"></script>
+<script type="text/javascript" src="skulliance.js?var=<?php echo rand(0,999); ?>"></script>
+</html>
