a3e88c9c1ebd98c8223cfa258dce9f398401e257
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-11-20 00:57:26 -0600

    Update db.php

--- db.php
+++ db.php
@@ -414,6 +414,14 @@ function getNFTs($conn, $filterby="", $all=false, $diamond_skull=false, $diamond
 				</form>
 				<?php
 			}
+			if($core_projects == true){
+				?>
+				<form id="coreProjectForm" action="diamond-skulls.php" method="post">
+				  <input type="hidden" id="nft_id" name="nft_id" value="<?php echo $row["nfts_id"];?>">
+				  <input type="submit" value="Select" class="small-button">
+				</form>
+				<?php
+			}
 			echo "</div></div>";
 		  }
 		} else {
