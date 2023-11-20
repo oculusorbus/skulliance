cde4e58c4c14252629894527136e4ea906d1e3f7
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-11-20 02:14:01 -0600

    Update diamond-skulls.php

--- diamond-skulls.php
+++ diamond-skulls.php
@@ -118,10 +118,9 @@ include 'header.php';
 		<div id="nfts" class="nfts">
 			<?php 
 			if(isset($_SESSION['userData']['user_id'])){ 
-				if($filterby == "" || $filterby == "None"){
-					$filterby = 1;
+				if($filterby != "" && $filterby != "None"){
+					getNFTs($conn, $filterby, $all=false, $diamond_skull=false, $diamond_skull_id="", $core_projects=true); 
 				}
-				getNFTs($conn, $filterby, $all=false, $diamond_skull=false, $diamond_skull_id="", $core_projects=true); 
 			}else{
 				echo "<p>You do not own any qualifying NFTs.<br><br>Please connect a Cardano wallet to view your NFTs.</p>";
 			} 
