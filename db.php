720df737e786f96a7500393f793d8df5b0ba1a6d
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-11-20 16:56:41 -0600

    Update db.php

--- db.php
+++ db.php
@@ -389,6 +389,7 @@ function getDiamondSkullNFTs($conn, $diamond_skull_id, $project_id, $projects, $
 	    echo "<div class='diamond'><div class='diamond-data'>";
 		echo "<span class='nft-name'>".substr($row["asset_name"], 0, 19)."</span>";
 		renderIPFS($row["ipfs"], $row["collection_id"]);
+		echo "<span class='nft-level'><strong>Owner</strong><br>".$row["username"]."</span>";
 		if($_SESSION['userData']['user_id'] == $row["user_id"] || $diamond_skull_owner == true){
 		?>
 			<form id="nftRemovalForm" action="diamond-skulls.php" method="post">
@@ -396,7 +397,6 @@ function getDiamondSkullNFTs($conn, $diamond_skull_id, $project_id, $projects, $
 			  <input type="submit" value="Remove" class="small-button">
 			</form>
 		<?php
-		echo "<span class='nft-level'><strong>Owner</strong><br>".$row["username"]."</span>";
 		}
 		echo "</div></div>";
 	  }
