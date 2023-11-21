33b5f4c6337e5e41836a503a3d4d377c2c330d59
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-11-21 09:47:52 -0600

    Update db.php

--- db.php
+++ db.php
@@ -542,14 +542,8 @@ function sendDiamondSkullNFTNotification($conn, $diamond_skull_id, $nft_id, $act
 		$verbiage = " Removed from ";
 	}
 	$title = $nft_name.$verbiage.$diamond_skull_name;
-	$description = $nft_owner."'s ".$nft_name.$verbiage.$diamond_skull_name." owned by ".$diamond_skull_owner;
+	$description = $nft_owner."'s ".$nft_name.$verbiage.$diamond_skull_owner."'s ".$diamond_skull_name;
 	$imageurl = renderIPFS($nft_image, $collection_id);
-	
-	echo $title;
-	echo $description;
-	echo $imageurl;
-	exit;
-	
 	discordmsg($title, $description, $imageurl, "https://skulliance.io/staking");
 }
 
