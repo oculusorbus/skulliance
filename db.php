7708c2621b03eaa237ce6e8484ed2b30f51ca414
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-11-20 23:30:35 -0600

    Update db.php

--- db.php
+++ db.php
@@ -516,7 +516,7 @@ function getDiamondSkullTotals($conn){
 		  	$diamond_skull_totals[$row["diamond_skull_id"]] = array();
 	  	  }
 		  if(!isset($diamond_skull_totals[$row["diamond_skull_id"]][$row["project_id"]])){
-		  	$diamond_skull_totals[$row["diamond_skull_id"]][$row["project_id"]] = array();
+		  	$diamond_skull_totals[$row["diamond_skull_id"]][$row["project_id"]] = 1;
 		  }
 		  $diamond_skull_totals[$row["diamond_skull_id"]][$row["project_id"]]++;
 	  }
