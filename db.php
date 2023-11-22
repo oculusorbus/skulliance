5537320389ad176a0edb4e7555de1a8674592bb9
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-11-22 03:34:46 -0600

    Update db.php

--- db.php
+++ db.php
@@ -716,6 +716,7 @@ function getNFTs($conn, $filterby="", $advanced_filter="", $diamond_skull=false,
 					echo "</table>";
 					echo "<span class='nft-level'><br><strong>CARBON Rewards</strong>: ".$delegator_rewards[$row["nfts_id"]]." of 38</span>";
 				}else{
+					echo "<span class='nft-level'><br><strong>All Slots Available</strong></span>";
 					echo "<span class='nft-level'><br><strong>CARBON Rewards</strong>: 0 of 38</span>";
 				}
 			}
