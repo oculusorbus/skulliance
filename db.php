216bc4c391cdca817543628736a3247742dc7e85
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-11-22 03:51:27 -0600

    Update db.php

--- db.php
+++ db.php
@@ -387,7 +387,7 @@ function renderIPFS($ipfs, $collection_id, $ipfs_format){
 
 // Get NFTs associated with a Diamond Skull
 function getDiamondSkullNFTs($conn, $diamond_skull_id, $project_id, $projects, $project_names){
-	$sql = "SELECT nfts.id AS nfts_id, asset_name, nfts.name AS nfts_name, ipfs, collections.id AS collection_id, projects.name AS project_name, nfts.user_id AS user_id, users.username AS username FROM diamond_skulls INNER JOIN nfts ON nfts.id = diamond_skulls.nft_id INNER JOIN collections ON nfts.collection_id = collections.id INNER JOIN projects ON collections.project_id = projects.id JOIN users ON users.id = nfts.user_id WHERE diamond_skulls.diamond_skull_id = '".$diamond_skull_id."' AND collections.project_id = '".$project_id."'";
+	$sql = "SELECT nfts.id AS nfts_id, asset_name, nfts.name AS nfts_name, ipfs, collections.id AS collection_id, projects.name AS project_name, nfts.user_id AS user_id, users.username AS username, rate FROM diamond_skulls INNER JOIN nfts ON nfts.id = diamond_skulls.nft_id INNER JOIN collections ON nfts.collection_id = collections.id INNER JOIN projects ON collections.project_id = projects.id JOIN users ON users.id = nfts.user_id WHERE diamond_skulls.diamond_skull_id = '".$diamond_skull_id."' AND collections.project_id = '".$project_id."'";
 	$result = $conn->query($sql);
 	
 	$diamond_skull_owner = verifyDiamondSkullOwner($conn, $diamond_skull_id);
@@ -410,6 +410,7 @@ function getDiamondSkullNFTs($conn, $diamond_skull_id, $project_id, $projects, $
 			</form>
 		<?php
 		}
+		echo "<span class='nft-level'><strong>".$row["rate"]." CARBON</strong></span>";
 		echo "</div></div>";
 	  }
 	} else {
