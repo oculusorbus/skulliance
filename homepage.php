950bb776cfaf0eeb233c5bb1dd4551ffc97c8ea6
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-11-24 22:53:46 -0600

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -16,7 +16,7 @@
 	  }
 	  .banner {
 	  	  background-image: url('images/skulliancebackground.png');
-		  background-color: 777;
+		  background-color: #36393F;
 		  background-blend-mode: multiply;
 		  background-size: cover;
 		  background-repeat: no-repeat;
