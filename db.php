0d1282520327823a8c65cc8f2cd4d3083880cba3
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-11-26 23:22:34 -0600

    Update db.php

--- db.php
+++ db.php
@@ -746,6 +746,8 @@ function getNFTs($conn, $filterby="", $advanced_filter="", $diamond_skull=false,
 			$core_where = "AND nfts.id NOT IN(SELECT nft_id FROM diamond_skulls)";
 		}
 		$sql = "SELECT asset_name, nfts.name AS nfts_name, ipfs, collection_id, nfts.id AS nfts_id, collections.rate AS rate, projects.currency AS currency, projects.id AS project_id, projects.name AS project_name, collections.name AS collection_name, users.username AS username FROM nfts INNER JOIN users ON users.id = nfts.user_id INNER JOIN collections ON nfts.collection_id = collections.id INNER JOIN projects ON collections.project_id = projects.id WHERE ".$user_filter.$and.$filterby.$diamond_skull_filter.$core_where." ORDER BY project_id, collection_id";
+		echo $sql;
+		exit;
 		$result = $conn->query($sql);
 
 		if ($result->num_rows > 0) {
