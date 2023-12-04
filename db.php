1e7fe50efeda94d025153399cf1d07f9bca686de
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-12-04 14:43:58 -0600

    Update db.php

--- db.php
+++ db.php
@@ -752,6 +752,7 @@ function getNFTs($conn, $filterby="", $advanced_filter="", $diamond_skull=false,
 		}
 		$sql = "SELECT asset_name, nfts.name AS nfts_name, ipfs, collection_id, nfts.id AS nfts_id, collections.rate AS rate, projects.currency AS currency, projects.id AS project_id, projects.name AS project_name, collections.name AS collection_name, users.username AS username FROM nfts INNER JOIN users ON users.id = nfts.user_id INNER JOIN collections ON nfts.collection_id = collections.id INNER JOIN projects ON collections.project_id = projects.id WHERE ".$user_filter.$and.$filterby.$diamond_skull_filter.$core_where." ORDER BY project_id, collection_id";
 		$result = $conn->query($sql);
+		echo $sql;
 
 		if ($result->num_rows > 0) {
 		  // output data of each row
