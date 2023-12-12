ea87035062fba81a1fe383ce3ae1d861f6d42bfc
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-12-11 23:23:20 -0600

    Update universe.php

--- universe.php
+++ universe.php
@@ -13,7 +13,6 @@ include 'header.php';
 	background-image: url('images/space.jpg');
 	height: 100%;
 	background-position: center;
-	background-attachment: scroll;
 }	
 .col1of3{
 	background-color: transparent;
