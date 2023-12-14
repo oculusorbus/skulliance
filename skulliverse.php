ffbdffa12d1c4a077c0eeb6496d622bfdc704af1
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-12-14 15:37:11 -0600

    Update skulliverse.php

--- skulliverse.php
+++ skulliverse.php
@@ -148,6 +148,12 @@ include 'header.php';
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
   animation-name: animatetop;
   animation-duration: 0.4s
+  min-height: 200px;
+}
+
+.modal-image{
+	max-width: 200px;
+	float: left;
 }
 
 /* Add Animation */
