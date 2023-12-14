1821181a59b3fbf89f94e66d22336dc90df60e9a
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-12-14 14:47:56 -0600

    Update skulliverse.php

--- skulliverse.php
+++ skulliverse.php
@@ -108,11 +108,11 @@ include 'header.php';
 
 /* Modal Content/Box */
 .modal-content {
-  background-color: #fefefe;
+  background-color: #000;
   margin: 15% auto; /* 15% from the top and centered */
   padding: 20px;
   border: 1px solid #888;
-  width: 80%; /* Could be more or less, depending on screen size */
+  width: 50%; /* Could be more or less, depending on screen size */
 }
 
 /* The Close Button */
