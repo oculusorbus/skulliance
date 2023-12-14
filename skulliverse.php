d4f38ab77276d705f57bdf8cf3a154684604e1c0
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-12-14 16:13:46 -0600

    Update skulliverse.php

--- skulliverse.php
+++ skulliverse.php
@@ -246,7 +246,7 @@ if($filterby != ""){
     //alert(pixels);
 	for (var i = 0; i < planets.length; i++) {
 		if(window.innerWidth <= 700){
-			planets.item(i).style.paddingTop = (window.innerHeight*1.5)+"px";
+			planets.item(i).style.paddingTop = (window.innerHeight*1)+"px";
 		}else{
 	   	 	planets.item(i).style.paddingTop = pixels;
    		}
