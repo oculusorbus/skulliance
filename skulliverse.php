f5d544db9b721ffa5abd42023db0af6fb73c7826
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-12-14 23:40:23 -0600

    Update skulliverse.php

--- skulliverse.php
+++ skulliverse.php
@@ -296,11 +296,11 @@ include 'header.php';
 				</div>
 				<img id="modal-image" src=""/>
 			    <p id="modal-text"></p>
-				<br><br>
-				<form id="delegationForm" action="diamond-skulls.php" method="post">
+				<p>				<form id="delegationForm" action="diamond-skulls.php" method="post">
 				  Convert the following amount of core project currency to DIAMOND:<br><br>
 				  <input type="submit" value="Delegate" class="button">
 				</form>
+				</p>
 			  </div>
 			  <div class="modal-footer">
 			    <h3></h3>
