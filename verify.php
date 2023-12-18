2633edc81def1498b867e78eed0be0794fdf6557
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-12-17 20:04:18 -0600

    Update verify.php

--- verify.php
+++ verify.php
@@ -86,10 +86,10 @@ function verifyNFTs($conn, $addresses, $policies){
 									$nft = $metadata->$policy_id;
 									if(isset($nft)){
 										if(str_contains($asset_name, "Hades")){
+											echo $policy_id;
 											echo $asset_name;
 											print_r($nft);
-										}else{
-											break;
+											exit;
 										}
 										$nft_data = $nft->$asset_name;
 										if(isset($nft_data)){
