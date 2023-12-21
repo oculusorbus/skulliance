838d5fba51326bf2c25430d1eaa59163d9701d3b
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-12-21 15:25:29 -0600

    Update verify.php

--- verify.php
+++ verify.php
@@ -65,10 +65,7 @@ function verifyNFTs($conn, $addresses, $policies, $asset_ids){
 				} // End if
 			} // End foreach
 			
-			print_r($asset_list);
-			exit;
-			
-			$tokench = curl_init("https://api.koios.rest/api/v0/asset_info");
+			$tokench = curl_init("https://api.koios.rest/api/v1/asset_info");
 			curl_setopt( $tokench, CURLOPT_HTTPHEADER, array('Content-type: application/json', 'authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZGRyIjoic3Rha2UxdXlxc3p2dDhjazlmaGVtM3o2M2NqNXpkaGRxem53aGtuczVkeDc1YzNjcDB6Z3MwODR1OGoiLCJleHAiOjE3MzQ3MDc5OTUsInRpZXIiOjEsInByb2pJRCI6InNrdWxsaWFuY2UifQ.eYZU74nwkN_qD8uK0UIv9VLveZLXMfJHznvzPWmnrq0'));
 			curl_setopt( $tokench, CURLOPT_POST, 1);
 			curl_setopt( $tokench, CURLOPT_POSTFIELDS, json_encode($asset_list));
@@ -81,6 +78,10 @@ function verifyNFTs($conn, $addresses, $policies, $asset_ids){
 			$tokenresponse = curl_exec( $tokench );
 			$tokenresponse = json_decode($tokenresponse);
 			curl_close( $tokench );
+			
+			print_r($tokenresponse);
+			exit;
+			
 			if(is_array($tokenresponse)){
 				foreach($tokenresponse AS $index => $tokenresponsedata){
 					if(isset($tokenresponsedata->minting_tx_metadata)){
