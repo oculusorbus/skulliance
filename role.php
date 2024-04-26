f5d8d8188aeb6ecdc6722327f24e376ff3b9c152
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-04-26 18:17:19 -0500

    Update role.php

--- role.php
+++ role.php
@@ -28,14 +28,12 @@ function assignRole($discord_id, $role_id, $action="") {
 	$response = curl_exec($ch);
 
 	//It's possible to output the response at this place for debugging, so remove the comment if needed
-	if($request == "DELETE"){
+/*
 	print $response;
 	print "<pre>";
 	print_r(json_decode($response));
 	print "</pre>";
-	exit;
-	}
-
+*/
 	curl_close($ch);
 }
 ?>
