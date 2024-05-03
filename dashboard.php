6229dcbbd033fa7a848eddca4fd1a3f4d0694ec7
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-05-03 17:35:46 -0500

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -13,11 +13,11 @@ include 'header.php';
 	<div class="modal-content">
 	  <div class="modal-header">
 	    <span class="close">&times;</span>
-	    <h2 id="modal-header">ANNOUNCEMENT</h2>
+	    <h2 id="modal-header">IMPORTANT ANNOUNCEMENT</h2>
 	  </div>
 	  <div class="modal-body">
 	    <p id="modal-text">
-			Leaderboards have been updated to allow for stakers to view other staker's NFT collections by clicking on usernames from the leaderboards. You have a choice as to whether you want other stakers to view your NFT collection or not. Select 'Visible' to allow. Select 'Hidden' to reject. This setting can be changed at any time under the Wallets menu item and is set to 'Hidden' by default.
+			Leaderboards have been updated to allow for stakers to view other staker's NFT collections by clicking on usernames from the leaderboards. You have a choice as to whether you want other stakers to view your NFT collection or not. Select 'Visible' to allow. Select 'Hidden' to reject. This setting can be changed at any time under the Wallets menu item and is set to 'Hidden' by default. This message will continue to be displayed on the dashboard until a selection is made and submitted.
 			<?php if(isset($_SESSION['userData']['user_id'])){ renderVisibility("dashboard"); } ?>
 		</p>
 	  </div>
