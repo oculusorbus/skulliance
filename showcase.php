fbba068df379bef327ff5d980b49ce83df4471ed
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-05-03 14:03:56 -0500

    Update showcase.php

--- showcase.php
+++ showcase.php
@@ -16,7 +16,7 @@ if(isset($_GET['username'])){
 <!-- The flexible grid (content) -->
 <div class="row" id="row1">
   <div class="main">
-	<h2>Qualifying NFTs</h2>
+	<h2><?php echo ucfirst($username); ?> NFTs</h2>
 	<a name="holdings" id="holdings"></a>
     <div class="content">
 		<?php filterNFTs("showcase", $username); ?>
