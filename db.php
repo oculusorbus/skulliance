a6686fd0ac5e92daaa7e1ee2a28050e4b413cad6
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-05-17 01:38:52 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1713,153 +1713,6 @@ function getProjectDelegationPercentages($conn){
 	return $percentages;
 }
 
-function confetti(){
-	echo '<script src="https://cdn.jsdelivr.net/npm/@tsparticles/confetti@3.0.3/tsparticles.confetti.bundle.min.js"></script>';
-	echo '<script type="text/javascript">
-		  tsParticles.load({
-          id: "tsparticles",
-          options: {
-  "fullScreen": {
-    "zIndex": 1
-  },
-  "emitters": [
-    {
-      "position": {
-        "x": 0,
-        "y": 30
-      },
-      "rate": {
-        "quantity": 5,
-        "delay": 0.15
-      },
-      "particles": {
-        "move": {
-          "direction": "top-right",
-          "outModes": {
-            "top": "none",
-            "left": "none",
-            "default": "destroy"
-          }
-        }
-      }
-    },
-    {
-      "position": {
-        "x": 100,
-        "y": 30
-      },
-      "rate": {
-        "quantity": 5,
-        "delay": 0.15
-      },
-      "particles": {
-        "move": {
-          "direction": "top-left",
-          "outModes": {
-            "top": "none",
-            "right": "none",
-            "default": "destroy"
-          }
-        }
-      }
-    }
-  ],
-  "particles": {
-    "color": {
-      "value": [
-        "#ffffff",
-        "#FF0000"
-      ]
-    },
-    "move": {
-      "decay": 0.05,
-      "direction": "top",
-      "enable": true,
-      "gravity": {
-        "enable": true
-      },
-      "outModes": {
-        "top": "none",
-        "default": "destroy"
-      },
-      "speed": {
-        "min": 10,
-        "max": 50
-      }
-    },
-    "number": {
-      "value": 0
-    },
-    "opacity": {
-      "value": 1
-    },
-    "rotate": {
-      "value": {
-        "min": 0,
-        "max": 360
-      },
-      "direction": "random",
-      "animation": {
-        "enable": true,
-        "speed": 30
-      }
-    },
-    "tilt": {
-      "direction": "random",
-      "enable": true,
-      "value": {
-        "min": 0,
-        "max": 360
-      },
-      "animation": {
-        "enable": true,
-        "speed": 30
-      }
-    },
-    "size": {
-      "value": {
-        "min": 0,
-        "max": 2
-      },
-      "animation": {
-        "enable": true,
-        "startValue": "min",
-        "count": 1,
-        "speed": 16,
-        "sync": true
-      }
-    },
-    "roll": {
-      "darken": {
-        "enable": true,
-        "value": 25
-      },
-      "enable": true,
-      "speed": {
-        "min": 5,
-        "max": 15
-      }
-    },
-    "wobble": {
-      "distance": 30,
-      "enable": true,
-      "speed": {
-        "min": -7,
-        "max": 7
-      }
-    },
-    "shape": {
-      "type": [
-        "circle",
-        "square"
-      ],
-      "options": {}
-    }
-  }
-}
-      }); </script>';
-}
-
 // Check leaderboard for discord and site display
 function checkLeaderboard($conn, $clean, $project_id=0) {
 	$where = "";
@@ -1899,14 +1752,12 @@ function checkLeaderboard($conn, $clean, $project_id=0) {
 				if($leaderboardCounter == 1){
 					//$width = 50;
 					$trophy = "<img style='width:".$width."px' src='/staking/icons/first.png' class='icon'/>";
-					confetti();
 				}else if($leaderboardCounter == 2){
 					//$width = 45;
 					if($last_total != $row["total"]){
 						$trophy = "<img style='width:".$width."px' src='/staking/icons/second.png' class='icon'/>";
 					}else{
 						$trophy = "<img style='width:".$width."px' src='/staking/icons/first.png' class='icon'/>";
-						confetti();
 						$leaderboardCounter--;
 					}
 				}else if($leaderboardCounter == 3){
