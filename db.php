8767935c8060581fbc5f229b876ae60b220060f2
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-05-17 01:11:19 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1796,7 +1796,7 @@ function checkLeaderboard($conn, $clean, $project_id=0) {
 					$username = $row["username"];
 				}
 				echo "<tr class='".$highlight."'>";
-		    	echo "<td align='center'><td>".$rowCounter."</td><strong>".(($trophy == "")?(($leaderboardCounter<10)?"0":"").$leaderboardCounter.".":$trophy)."</strong></td><td align='center'>".$avatar."</td><td><strong style='font-size:20px'>".$username."</strong></td><td align='center'>".$row["total"].$delegated."</td><td align='center'>".(($project_id != 0)?" ".number_format($current_balance)." ".$row["currency"]."":"").$diamond_skull_count."</td>";
+		    	echo "<td align='center'>".$rowCounter."</td><td align='center'><strong>".(($trophy == "")?(($leaderboardCounter<10)?"0":"").$leaderboardCounter.".":$trophy)."</strong></td><td align='center'>".$avatar."</td><td><strong style='font-size:20px'>".$username."</strong></td><td align='center'>".$row["total"].$delegated."</td><td align='center'>".(($project_id != 0)?" ".number_format($current_balance)." ".$row["currency"]."":"").$diamond_skull_count."</td>";
 				echo "</tr>";
 				$last_total = $row["total"];
 		  	}
