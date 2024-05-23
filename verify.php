b919786bae94755c823e1c5ac7d68116596546ed
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-05-23 13:07:45 -0500

    Query Response Test

--- verify.php
+++ verify.php
@@ -79,6 +79,8 @@ function verifyNFTs($conn, $addresses, $policies, $asset_ids){
 				} // End foreach
 			}
 			
+			print_r($asset_list);
+			
 			$tokench = curl_init("https://api.koios.rest/api/v1/asset_info");
 			curl_setopt( $tokench, CURLOPT_HTTPHEADER, array('Content-type: application/json', 'authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZGRyIjoic3Rha2UxdXlxc3p2dDhjazlmaGVtM3o2M2NqNXpkaGRxem53aGtuczVkeDc1YzNjcDB6Z3MwODR1OGoiLCJleHAiOjE3MzQ3MDc5OTUsInRpZXIiOjEsInByb2pJRCI6InNrdWxsaWFuY2UifQ.eYZU74nwkN_qD8uK0UIv9VLveZLXMfJHznvzPWmnrq0'));
 			curl_setopt( $tokench, CURLOPT_POST, 1);
@@ -94,6 +96,8 @@ function verifyNFTs($conn, $addresses, $policies, $asset_ids){
 			curl_close( $tokench );
 			
 			if(is_array($tokenresponse)){
+				print_r($tokenresponse);
+				
 				foreach($tokenresponse AS $index => $tokenresponsedata){
 					// Check whether NFT already exists in the db. If so, just update it and don't fuck with cycling through NFT metadata that tends to randomly fail
 					if(in_array($tokenresponsedata->fingerprint, $asset_ids)){
