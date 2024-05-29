ac2ec563fafca2b19961be68251fc2354fc209f0
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-05-28 23:17:31 -0500

    Update skulliverse.php

--- skulliverse.php
+++ skulliverse.php
@@ -278,9 +278,9 @@ if($filterby != ""){
 		 "<strong>Planet Type:</strong> "+category+
 		 "<br><br><strong>Inhabitants:</strong> "+inhabitants+
  		 "<br><br><strong>Currency:</strong> "+currency+
+		 "<br><br><strong>Population:</strong> "+population+
 		 "<br><br><strong>Delegations:</strong> "+delegations+
-		 "<br><br><strong>Delegators:</strong> "+delegators+
-		 "<br><br><strong>Population:</strong> "+population;
+		 "<br><br><strong>Delegators:</strong> "+delegators;
 		 
 		 if(project_id != 7){
 			 document.getElementById('modal-text').innerHTML = document.getElementById('modal-text').innerHTML+"<br><br><strong>2x CARBON Rewards:</strong> "+status.toUpperCase();
