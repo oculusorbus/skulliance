df1bf5fc0e7dd9444d7f0fb6356ed7829a7dac3c
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-01 20:32:27 -0500

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -204,7 +204,7 @@
 			<img src="images/projects/deadpophell.png"/>
 		</div>
 		<div class="project">
-			<img src="images/projects/jordi.png"/>
+			<img src="images/projects/pendulum.jpg"/>
 		</div>
 	</div>
 </div>
@@ -214,10 +214,24 @@
 			<img src="images/projects/danketsu.png"/>
 		</div>
 		<div class="project">
-			<img src="images/projects/cardanocamera.jpg"/>
+			<img src="images/projects/jordi.png"/>
 		</div>
 		<div class="project">
 			<img src="images/projects/apprentices.png"/>
+		</div>
+	</div>
+</div>
+<div class="row project-row" id="row1">
+	<div class="projects">
+
+		<div class="project">
+			<img src="images/projects/cardanocamera.jpg"/>
+		</div>
+		<div class="project">
+			<img src="images/projects/mipatoys.png"/>
+		</div>
+		<div class="project">
+			<img src="images/projects/ascenderone.jpg"/>
 		</div>
 	</div>
 </div>
