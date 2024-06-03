d7ff2cdae7010cf8ea1ce2aadb7eddfccb0a3747
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-03 00:01:12 -0500

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -53,7 +53,7 @@ if(getVisibility($conn) == "0"){
 				<?php
 				$days = getStreakRewards($conn);
 				if(!empty($days)){
-					echo "<h2>Current Streak</h2>";
+					echo "<h3>Current Streak</h3>";
 					foreach($days AS $number => $day){
 						echo "<li class='role'>";
 						echo "Day ".$number." - ".$day["amount"]." ".$day["currency"];
