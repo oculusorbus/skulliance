74728a0ad32e28a5522c0509c620be273b0e7a52
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-03 21:48:36 -0500

    Update skulliance.php

--- skulliance.php
+++ skulliance.php
@@ -466,7 +466,7 @@ function renderDailyRewardsSection(){
 		echo '<li class="role">'; 
 		$d1 = new DateTime(getMaxDateCreated($conn));
 		//$d1 = new DateTime(date('Y-m-d H:i:s'));  
-		$d2 = new DateTime(strtotime('+1 day', strtotime(getMaxDateCreated($conn)));
+		$d2 = new DateTime(strtotime('+1 day', strtotime(getMaxDateCreated($conn))));
 		$interval = $d1->diff($d2);
 		echo $interval->format('%h')." hours and ".$interval->format('%i')." minutes until next reward.";
 		echo '</li>';
