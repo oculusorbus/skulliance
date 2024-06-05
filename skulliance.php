cfd05f7f83777c20a448a10a7b6db7f57c85505c
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-05 13:56:42 -0500

    Update skulliance.php

--- skulliance.php
+++ skulliance.php
@@ -458,7 +458,7 @@ function renderDailyRewardsSection(){
 		echo '<strong>Daily Reward Already Claimed</strong>';
 		echo '</li>';
 		echo '<li class="role" id="remaining" style="display:none;">';
-		echo '</li>';
+		echo '</li><br>';
 		echo '<input id="claimRewardButton" type="button" value="Claim Daily Reward" class="button" onclick="javascript:dailyReward();">';
 	} else { 
 			// Display 7 day completed rewards despite streak being reset
