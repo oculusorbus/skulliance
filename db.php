c5d30de2ac17963c93f1c727fe50e889d9fec789
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-06 15:11:45 -0500

    Update db.php

--- db.php
+++ db.php
@@ -548,7 +548,9 @@ function getMissions($conn, $quest_id) {
 
 // Get Current Missions for User
 function getCurrentMissions($conn){
-	$sql = "SELECT DISTINCT missions.id, title, COUNT(nft_id) AS total_nfts, SUM(rate) AS success_rate FROM missions INNER JOIN quests ON missions.quest_id = quests.id INNER JOIN missions_nfts ON missions.id = missions_nfts.mission_id INNER JOIN nfts ON nfts.id = missions_nfts.nft_id WHERE status = 0 AND user_id = '".$_SESSION['userData']['user_id']."' GROUP BY missions.id ORDER BY missions.id ASC";
+	$sql = "SELECT DISTINCT missions.id, title, COUNT(nft_id) AS total_nfts, SUM(rate) AS success_rate FROM missions 
+	INNER JOIN quests ON missions.quest_id = quests.id INNER JOIN missions_nfts ON missions.id = missions_nfts.mission_id INNER JOIN nfts ON nfts.id = missions_nfts.nft_id INNER JOIN collections ON collections.id = nfts.collection_id 
+	WHERE status = 0 AND user_id = '".$_SESSION['userData']['user_id']."' GROUP BY missions.id ORDER BY missions.id ASC";
 	
 	$result = $conn->query($sql);
 	
