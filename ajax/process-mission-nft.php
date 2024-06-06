4ea013bda2278a9ee6ee307485aa00a298545c32
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-06 16:47:27 -0500

    Update process-mission-nft.php

--- ajax/process-mission-nft.php
+++ ajax/process-mission-nft.php
@@ -8,6 +8,8 @@ if(isset($_GET['action']) && isset($_GET['nft_id']) && isset($_GET['rate'])){
 	}else if($_GET['action'] == "Deselect"){
 		unset($_SESSION['userData']['mission']['nfts'][$_GET['nft_id']]);
 	}
+}else{
+	echo "No Session";
 }
 //$_SESSION['userData']['mission']['nfts'][$row["id"]] = $row["rate"];
 
