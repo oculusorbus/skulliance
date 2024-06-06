9653ba610058fb1893c2be109988cb43e0df7fc1
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-06 14:05:27 -0500

    Update db.php

--- db.php
+++ db.php
@@ -578,8 +578,8 @@ function getInventory($conn, $project_id, $quest_id) {
 function startMission($conn){
 	if(isset($_SESSION['userData']['mission']['quest_id']) && isset($_SESSION['userData']['mission']['user_id'])){
 		$sql = "INSERT INTO missions (quest_id, user_id)
-		VALUES ('".$_SESSION['userData']['mission']['quest_id']."', '".$_SESSION['userData']['user_id']."');
-		SELECT LAST_INSERT_ID() AS mission_id;"
+		VALUES ('".$_SESSION['userData']['mission']['quest_id']."', '".$_SESSION['userData']['user_id']."'); 
+		SELECT LAST_INSERT_ID() AS mission_id;";
 		
 		/*
 		$mission_id = "";
