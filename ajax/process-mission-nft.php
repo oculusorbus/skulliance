1c5823e217168a374b3ab561dd00d463bcd39105
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-06 16:51:42 -0500

    Update process-mission-nft.php

--- ajax/process-mission-nft.php
+++ ajax/process-mission-nft.php
@@ -5,9 +5,10 @@ include '../skulliance.php';
 if(isset($_GET['action']) && isset($_GET['nft_id']) && isset($_GET['rate'])){
 	if($_GET['action'] == "Select"){
 		$_SESSION['userData']['mission']['nfts'][$_GET['nft_id']] = $_GET['rate'];
+		print_r($_SESSION['userData']['mission']);
 	}else if($_GET['action'] == "Deselect"){
 		unset($_SESSION['userData']['mission']['nfts'][$_GET['nft_id']]);
-		print_r($_SESSION);
+		print_r($_SESSION['userData']['mission']);
 	}
 }else{
 	echo "No Session";
