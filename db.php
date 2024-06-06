b705636fdad7afe71ae7b22fae76980d6db08998
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-06 11:20:37 -0500

    Update db.php

--- db.php
+++ db.php
@@ -561,9 +561,9 @@ function getInventory($conn, $project_id=0) {
 			echo substr($row["asset_name"], 0, 15)." (Rate ".$row["rate"].")";
 			$rate_tally += $row["rate"];
 			if($rate_tally <= 100){
-				echo "<input style='float:right' type='button' class='small-button' value='Deselect'/>";
+				echo "&nbsp;<input style='float:right' type='button' class='small-button' value='Deselect'/>";
 			}else{
-				echo "<input style='float:right' type='button' class='small-button' value='Select'/>";
+				echo "&nbsp;<input style='float:right' type='button' class='small-button' value='Select'/>";
 			}
 			echo "</li>";
 		}
