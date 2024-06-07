058f980f69bf9521a9fd709adc1668d9b58f2958
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-06 19:44:14 -0500

    Update complete-mission.php

--- ajax/complete-mission.php
+++ ajax/complete-mission.php
@@ -3,7 +3,7 @@ include '../db.php';
 include '../skulliance.php';
 
 if(isset($_GET['mission_id']) && isset($_GET['quest_id'])){
-	completeMission($conn, $mission_id, $quest_id);
+	completeMission($conn, $_GET['mission_id'], $_GET['quest_id']);
 }else{
 	echo "No Session";
 }
