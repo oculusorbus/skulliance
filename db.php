3c1e4f2bb082bb259971cc2b5f6a7bb9301ee266
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-07 17:16:15 -0500

    Update db.php

--- db.php
+++ db.php
@@ -666,10 +666,12 @@ function getInventory($conn, $project_id, $quest_id) {
 		echo "<li class='role no-border-style'><strong>Cost: ".$quest["cost"]." ".$quest["currency"]."</strong></li>";
 		echo "<li class='role no-border-style'><strong>Reward: ".$quest["reward"]." ".$quest["currency"]."</strong></li>";
 		echo "<li class='role no-border-style'><strong>Success Rate: </strong>&nbsp;<span id='success-rate'>Loading...</span>%</li>";
-		echo "<li class='role'>";
+		echo "<li class='role no-border-style'>";
 		echo "<input type='button' class='button' value='Start Mission' onclick='startMission();'/>";
 		echo "</li>";
-		echo "<li class='role'><strong>Inventory</strong></li>";
+		echo "</ul>";
+		echo "<h2>Inventory</strong></h2>";
+		echo "<ul>";
 		// Toggle Maximization and Balancing Inventory Selection Buttons
 		if($total_rates >= 100){
 			  if(!isset($_POST['maximize']) && !isset($_POST['balance'])){
