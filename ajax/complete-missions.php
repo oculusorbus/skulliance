247d0b7b8c4dd04b6e7bc1ca7408ff460166e061
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-08 01:36:22 -0500

    Update complete-missions.php

--- ajax/complete-missions.php
+++ ajax/complete-missions.php
@@ -7,7 +7,11 @@ if(isset($_GET['mission_id']) && isset($_GET['quest_id'])){
 	$quest_ids = array();
 	$mission_ids = explode(',', $_GET['mission_id']);
 	$quest_ids = explode(',', $_GET['quest_id']);
-	completeMission($conn, $_GET['mission_id'], $_GET['quest_id']);
+	$missions_quests = array();
+	$missions_quests = array_combine($mission_ids, $quest_ids);
+	foreach($mission_quests AS $mission_id => $quest_id){
+		completeMission($conn, $mission_id, $quest_id);
+	}
 }else{
 	echo "No Session";
 }
