ed1fc6fc38ce6ea42035281c5532bd7a9cc37ad3
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-07 20:13:17 -0500

    Update db.php

--- db.php
+++ db.php
@@ -527,7 +527,7 @@ function getMissions($conn, $quest_id) {
     	echo "<div class='nft'><div class='nft-data".$class."'>";
 		echo "<span class='nft-name'>".$row["title"]."</span>";
 		echo "<span class='nft-image'><img src='images/missions/".strtolower(str_replace(" ", "-", $row["title"])).".png'/></span>";
-		echo "<span class='nft-level'><strong>Description</strong><br>".$row["description"]."</span>";
+		//echo "<span class='nft-level'><strong>Description</strong><br>".$row["description"]."</span>";
 		echo "<span class='nft-level'><strong>Project</strong><br>".$row["name"]."</span>";
 		echo "<span class='nft-level'><strong>Cost</strong><br>".number_format($row["cost"])." ".$row["currency"]."</span>";
 		echo "<span class='nft-level'><strong>Reward</strong><br>".number_format($row["reward"])." ".$row["currency"]."</span>";
