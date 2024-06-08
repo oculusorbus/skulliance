9b0cbb5d2d6d597b78687e56db447f031ee5c8cb
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-08 01:50:38 -0500

    Update db.php

--- db.php
+++ db.php
@@ -877,9 +877,9 @@ function completeMission($conn, $mission_id, $quest_id){
 		if($success == 1){
 			updateBalance($conn, $_SESSION['userData']['user_id'], $project_id, $reward);
 			logCredit($conn, $_SESSION['userData']['user_id'], $reward, $project_id, 0, 0, 1);
-			echo "SUCCESS - ".$title." Mission was successful! Awarded ".$reward." ".$currency."\r\n\r\n";
+			echo "SUCCESS: ".$title." Mission - ".$reward." ".$currency."\r\n\r\n";
 		}else if($success == 2){
-			echo "FAILURE - ".$title." Mission failed. No ".$currency." Awarded\r\n\r\n";
+			echo "FAILURE: ".$title." Mission - NO ".$currency."\r\n\r\n";
 		}
 	}else{
 		echo "No Session";
