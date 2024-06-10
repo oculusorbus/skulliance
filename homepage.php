187a259a04a9d9d591b8331aa03217900cb4b3d5
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-09 21:56:23 -0500

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -326,7 +326,7 @@
 <div class="row teams-row" id="row1">
 	<div class="teams">
 		<div class="member">
-			<h3>Founder<br>Oculus Orbus</h3>
+			<h3><strong>Founder</strong><br>Oculus Orbus</h3>
 			<img src="https://www.skulliance.io/staking/images/team/oculusorbus.jpg"/>
 		</div>
 		<div class="member">
