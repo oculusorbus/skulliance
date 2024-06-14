94b28c94bc6f502aec4140cdf22acf2bbf7ce37f
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-14 11:53:39 -0500

    Update db.php

--- db.php
+++ db.php
@@ -2545,10 +2545,10 @@ function getTotalMissions($conn){
 			echo $row["total"];
 			echo "</td>";
 			echo "<td align='center'>";
-			echo $row["success"]." (".($row["success"]/($row["total"]))."%)";
+			echo $row["success"]." (".($row["success"]/($row["total"]-$row["progress"]))."%)";
 			echo "</td>";
 			echo "<td align='center'>";
-			echo $row["failure"]." (".($row["failure"]/($row["total"]))."%)";;
+			echo $row["failure"]." (".($row["failure"]/($row["total"]-$row["progress"]))."%)";;
 			echo "</td>";
 			echo "<td align='center'>";
 			echo $row["progress"];
