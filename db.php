7a3abf39940ba5a63bf589287684d7a62f9f29ee
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-14 23:56:00 -0500

    Update db.php

--- db.php
+++ db.php
@@ -2628,7 +2628,7 @@ function checkMissionsLeaderboard($conn, $monthly=false, $rewards=false){
 	$sql = "SELECT (SELECT COUNT(success_missions.id) FROM missions AS success_missions INNER JOIN users AS success_users ON success_users.id = success_missions.user_id WHERE success_missions.status = '1' AND success_users.id = users.id) AS success, 
 	               (SELECT COUNT(failed_missions.id) FROM missions AS failed_missions INNER JOIN users AS failed_users ON failed_users.id = failed_missions.user_id  WHERE failed_missions.status = '2' AND failed_users.id = users.id) AS failure, 
 				   (SELECT COUNT(progress_missions.id) FROM missions AS progress_missions INNER JOIN users AS progress_users ON progress_users.id = progress_missions.user_id  WHERE progress_missions.status = '0' AND progress_users.id = users.id) AS progress, 
-	        COUNT(missions.id) AS total, users.id AS user_id, username, avatar, discord_id, visibility, (total + success - failure - progress) AS score 
+	        COUNT(missions.id) AS total, users.id AS user_id, username, avatar, discord_id, visibility, (success - failure - progress) AS score 
 		    FROM users INNER JOIN missions ON missions.user_id = users.id INNER JOIN quests ON quests.id = missions.quest_id ".$where." GROUP BY users.id ORDER BY score DESC";
 	$result = $conn->query($sql);
 
