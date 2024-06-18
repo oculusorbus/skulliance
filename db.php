1f3643461ed71871d01c8c25d16f2c41ca4d286a
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-18 16:53:04 -0500

    Update db.php

--- db.php
+++ db.php
@@ -735,6 +735,7 @@ function getInventory($conn, $project_id, $quest_id) {
 		echo "<h2>Inventory</strong></h2>";
 		echo "<ul>";
 		
+		unset($_SESSION['userData']['mission']['nfts']);
 		unset($_SESSION['userData']['mission']['consumables']);
 		$consumables = array();
 		$consumables = getCurrentAmounts($conn);
