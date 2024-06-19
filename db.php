cc6c55bf07925ca704e88ba47505bb729b361120
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-18 22:46:46 -0500

    Update db.php

--- db.php
+++ db.php
@@ -769,7 +769,11 @@ function getInventory($conn, $project_id, $quest_id) {
 		$consumables = getCurrentAmounts($conn);
 		foreach($consumables AS $id => $consumable){
 			if($consumable["amount"] != 0){
-				echo "<li class='role'><input id='consumable-".$id."' type='button' class='small-button' value='Select' onclick='processConsumable(this.value, ".$id.");' />&nbsp;".$consumable["name"].":&nbsp;<span id='amount-".$id."'>".$consumable["amount"]."</span></li>";
+				echo "<li class='role'>";
+				echo "<input id='consumable-".$id."' type='button' class='small-button' value='Select' onclick='processConsumable(this.value, ".$id.");' />";
+				echo "<img class='icon' src='icons/".str_replace("%", "", str_replace(" ", "-", $consumable["name"])).".png'/>";
+				echo "&nbsp;".$consumable["name"].":&nbsp;<span id='amount-".$id."'>".$consumable["amount"]."</span>";
+				echo "</li>";
 			}
 		}
 		
