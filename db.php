d9bb52fa79184218b6174561be4de7c7b5b7da99
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-19 20:45:19 -0500

    Update db.php

--- db.php
+++ db.php
@@ -765,7 +765,7 @@ function getInventory($conn, $project_id, $quest_id) {
 		echo "</ul>";
 		echo "<h2>Inventory</strong></h2>";
 		echo "<ul>";
-		//echo "<li class='role'><input style='padding:12px;' type='button' class='small-button' value='Clear All Selections' onclick='clearAllSelections();'/></li>";
+		echo "<li class='role'><input style='padding:12px;' type='button' class='small-button' value='Clear All Selections' onclick='clearAllSelections();'/></li>";
 		
 		$consumables = array();
 		$consumables = getCurrentAmounts($conn);
@@ -808,7 +808,6 @@ function getInventory($conn, $project_id, $quest_id) {
 			echo "</li>";
 		}
 		echo "</ul>";
-		/*
 		if(!empty($nft_ids)){
 			echo "<script type='text/javascript'>";
 			echo "function clearAllSelections(){";
@@ -816,6 +815,7 @@ function getInventory($conn, $project_id, $quest_id) {
 			foreach($consumables AS $id => $consumable){
 				echo "document.getElementById('consumable-".$id."').value = 'Select';";
 				echo "document.getElementById('consumable-".$id."').classList.remove('activated');";
+				echo "document.getElementById('amount-".$id."').innerHTML = '".$consumable["amount"]."';";
 			}
 			
 			foreach($nft_ids AS $id => $nft_id){
@@ -828,7 +828,7 @@ function getInventory($conn, $project_id, $quest_id) {
 			echo "xhttp.send();";
 			echo "}";
 			echo "</script>";
-		}*/
+		}
 		return $rate_tally;
 	}
 }
