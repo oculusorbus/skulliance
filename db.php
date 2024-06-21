6e7ff4f85ec66f3a3db1c5baebb2e67a22ab9cc4
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-21 16:40:05 -0500

    Update db.php

--- db.php
+++ db.php
@@ -2858,7 +2858,7 @@ function checkMissionsLeaderboard($conn, $monthly=false, $rewards=false){
 	$sql = "SELECT (SELECT COUNT(success_missions.id) FROM missions AS success_missions INNER JOIN users AS success_users ON success_users.id = success_missions.user_id WHERE success_missions.status = '1' AND success_users.id = users.id) AS success, 
 	               (SELECT COUNT(failed_missions.id) FROM missions AS failed_missions INNER JOIN users AS failed_users ON failed_users.id = failed_missions.user_id  WHERE failed_missions.status = '2' AND failed_users.id = users.id) AS failure, 
 				   (SELECT COUNT(progress_missions.id) FROM missions AS progress_missions INNER JOIN users AS progress_users ON progress_users.id = progress_missions.user_id  WHERE progress_missions.status = '0' AND progress_users.id = users.id) AS progress, 
-	        COUNT(missions.id) AS total, users.id AS user_id, username, avatar, discord_id, visibility 
+	        COUNT(missions.id) AS total, users.id AS user_id, discord_id, username, avatar, discord_id, visibility 
 		    FROM users INNER JOIN missions ON missions.user_id = users.id INNER JOIN quests ON quests.id = missions.quest_id ".$where." GROUP BY users.id ORDER BY total DESC";
 	$result = $conn->query($sql);
 
@@ -2875,6 +2875,7 @@ function checkMissionsLeaderboard($conn, $monthly=false, $rewards=false){
 		$width = 40;
 		$missions = array();
 		$index = 0;
+		$description = "";
 		while($row = $result->fetch_assoc()) {
 			$missions[$index] = array();
 			$missions[$index]["visibility"] = $row["visibility"];
@@ -2987,7 +2988,14 @@ function checkMissionsLeaderboard($conn, $monthly=false, $rewards=false){
 			$last_total = $row["score"];
 			if($rewards){
 				logCredit($conn, $row["user_id"], round($carbon/$leaderboardCounter), 15);
+				$description .= $leaderboardCounter." <@".$row["discord_id"].">: ".$row["score"]."\r\n";
 			}
+		}
+		if($rewards){
+			$last_month = date('F', strtotime('last month'));
+			$title = $last_month." Missions Leaderboard";
+			$imageurl = "";
+			discordmsg($title, $description, $imageurl, "https://skulliance.io/staking");
 		}
 		echo "</table>";
 		if($fireworks){
