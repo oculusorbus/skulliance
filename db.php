46e0a64cff0bd2706fc25181d3b3a6dc019b2012
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-23 00:07:18 -0500

    Update db.php

--- db.php
+++ db.php
@@ -566,6 +566,8 @@ function getMissions($conn, $quest_id) {
 	$result = $conn->query($sql);
 	
 	$levels = getMissionLevels($conn);
+	print_r($levels);
+	exit;
 	if ($result->num_rows > 0) {
 	  // output data of each row
 	  while($row = $result->fetch_assoc()) {
