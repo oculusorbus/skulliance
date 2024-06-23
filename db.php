643efc9ea58ee43c1338266cdd8f61ecb8b53b3e
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-23 00:11:33 -0500

    Update db.php

--- db.php
+++ db.php
@@ -576,9 +576,7 @@ function getMissions($conn, $quest_id) {
 			if(isset($levels[$row["id"]])){
 				$max_level = $levels[$row["id"]];
 			}
-			echo $max_level+1;
-			exit;
-			if($max_level+1 >= $row["level"]){
+			if(($max_level+1) >= $row["level"]){
 	    		echo "<div class='nft'><div class='nft-data".$class." mission-data' onclick='document.getElementById(\"submit-".$row["id"]."\").click()'>";
 			}else{
 				echo "<div class='nft'><div class='nft-data".$class." mission-data' style='opacity:0.5'>";
