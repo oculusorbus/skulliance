149a72a2ebc55c7933924ee28381e0fb8124ad1c
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-28 04:34:10 -0500

    Update db.php

--- db.php
+++ db.php
@@ -599,6 +599,8 @@ function getMissionsFilters($conn, $quest_id) {
 function checkMissionInventory($conn, $project_id){
 	$sql = "SELECT id FROM nfts INNER JOIN collections ON collections.id = nfts.collection_id WHERE collections.project_id = '".$project_id."' AND user_id = '".$_SESSION['userData']['user_id']."'";
 	
+	$result = $conn->query($sql);
+	
 	if($result->num_rows > 0) {
 		return true;
 	}else{
