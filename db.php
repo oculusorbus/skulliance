2ce6bc8a3a2a32ca46c6511f3a36740bc7b9b560
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-28 11:38:23 -0500

    Update db.php

--- db.php
+++ db.php
@@ -567,7 +567,7 @@ function getMissionLevels($conn) {
 
 
 function getMissionsFilters($conn, $quest_id) {
-	$sql = "SELECT DISTINCT projects.id, projects.name FROM quests INNER JOIN projects ON projects.id = quests.project_id WHERE quests.id != '".$quest_id."' ORDER BY projects.name";
+	$sql = "SELECT DISTINCT projects.id, projects.name FROM quests INNER JOIN projects ON projects.id = quests.project_id ORDER BY projects.name";
 	
 	$result = $conn->query($sql);
 	
