69a76a8dc4e60e40f872498a6e39d808aa6dd271
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-28 11:01:12 -0500

    Update select-project-filter.php

--- ajax/select-project-filter.php
+++ ajax/select-project-filter.php
@@ -3,11 +3,7 @@ include '../db.php';
 include '../skulliance.php';
 
 if(isset($_GET['project_id'])){
-	if(checkMissionTotal($conn, $_GET['project_id'])){
-		$_SESSION['userData']['project_id'] = $_GET['project_id'];
-	}else{
-		unset($_SESSION['userData']['project_id']);
-	}
+	$_SESSION['userData']['project_id'] = $_GET['project_id'];
 }else{
 	echo "No Session";
 }
