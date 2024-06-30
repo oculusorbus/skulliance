355d317fe557d8daeaaaa3b11b74604bd236702d
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-30 02:46:19 -0500

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -66,6 +66,7 @@ if(getVisibility($conn) == "0"){
 	<h2>Qualifying NFTs</h2>
 	<a name="holdings" id="holdings"></a>
     <div class="content" id="filtered-content">
+		<?php echo $filterby; ?>
 		<?php filterNFTs("dashboard"); ?>
 		<div id="nfts" class="nfts">
 			<?php 
