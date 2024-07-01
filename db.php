798e21c0339c7c38986aaf4c8c809b0a49358d6a
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-07-01 00:16:04 -0500

    Update db.php

--- db.php
+++ db.php
@@ -3228,13 +3228,12 @@ function checkMissionsLeaderboard($conn, $monthly=false, $rewards=false){
 			if($rewards){
 				//updateBalance($conn, $row["user_id"], 15, round($carbon/$leaderboardCounter));
 				//logCredit($conn, $row["user_id"], round($carbon/$leaderboardCounter), 15);
-				$description .= "- ".(($leaderboardCounter<10)?"0":"").$leaderboardCounter." "."<@".$row["discord_id"]."> - Score: ".$row["score"].", Total: ".$row["total"]."\r\n";
+				$description .= "- ".(($leaderboardCounter<10)?"0":"").$leaderboardCounter." "."<@".$row["discord_id"]."> - ".$row["score"].", ".$row["total"]."\r\n";
 				//$description .= "        "."Success: ".$row["success"].", Failure: ".$row["failure"].", In Progress: ".$row["progress"]."\r\n";
 				$description .= "        ".number_format(round($carbon/$leaderboardCounter))." CARBON = ".number_format(floor(round($carbon/$leaderboardCounter)/100))." DIAMOND\r\n";
 			}
 		}
 		if($rewards){
-			echo $description;
 			$last_month = date('F', strtotime('last month'));
 			$title = $last_month." Missions Leaderboard Results";
 			$imageurl = "";
