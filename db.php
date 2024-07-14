02e17fc17331df0815ad8231f2e90bba916596c6
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-07-14 01:07:51 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1000,7 +1000,7 @@ function getInventory($conn, $project_id, $quest_id) {
 	}
 	$filename = "images/missions/".strtolower(str_replace(" ", "-", $quest["title"]));
 	echo "<li class='role no-border-style'>";
-	if($project_id != 3 && $project_id != 6 && $project_id != 4 && $project_id != 11 && $project_id != 22){
+	if($project_id != 3 && $project_id != 6 && $project_id != 4 && $project_id != 11 && $project_id != 22 && $project_id != 23){
 		echo "<img class='mission-image' width='100%' src='".$filename.".".$extension."'/>";
 	}else{
 		echo "<video width='100%' height='100%' controls autoplay loop muted poster='".$filename.".".$extension."'>
