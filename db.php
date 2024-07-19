9b6150ae2f6cb6f4a5f3ea8e256f74e3df9a013c
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-07-19 15:38:16 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1171,8 +1171,9 @@ function getQuestInfo($conn, $quest_id){
 }
 
 function startMission($conn){
-	if(!isset($_SESSION['userData']['mission']['nfts']) && !isset($_SESSION['userData']['mission']['consumables'])){
-		echo 'You cannot start a mission with 0% success rate.';
+	if(!isset($_SESSION['userData']['mission']['nfts']) && 
+	  (!isset($_SESSION['userData']['mission']['consumables'][1]) && !isset($_SESSION['userData']['mission']['consumables'][2]) && !isset($_SESSION['userData']['mission']['consumables'][3]) && !isset($_SESSION['userData']['mission']['consumables'][4]) )){
+		echo 'You cannot start a mission with no NFTs, 0% success rate, and no percentage success rate consumable items. A bug might have occurred. Please hard refresh the webpage and try again.';
 	}else{
 		if(isset($_SESSION['userData']['mission']['quest_id']) && isset($_SESSION['userData']['user_id'])){
 		
