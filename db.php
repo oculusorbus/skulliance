a14b43e8153b2e63808991021bae38ff0da7e628
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-10-08 00:23:38 -0500

    Update db.php

--- db.php
+++ db.php
@@ -946,7 +946,7 @@ function getCurrentMissions($conn){
 		  }
 		  $mission_ids = substr($mission_ids, 1);
 		  $quest_ids = substr($quest_ids, 1);
-		  echo "<br><input type='button' class='button' value='Claim All Completed Missions' onclick='completeMissions(\"".$mission_ids."\", \"".$quest_ids."\");this.style.display=\"none\";'/>";
+		  echo "<br><input type='button' class='button' value='Claim All Missions' onclick='completeMissions(\"".$mission_ids."\", \"".$quest_ids."\");this.style.display=\"none\";'/>";
   	  }
 	  renderStartAllFreeEligibleMissionsButton($conn);
 	  echo "</div>";
