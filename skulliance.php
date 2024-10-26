cb5ef20493194bd4dc9b894ce08bebc86e5e0494
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-10-26 00:46:37 -0500

    Update skulliance.php

--- skulliance.php
+++ skulliance.php
@@ -3,6 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+print_r($_SESSION);
+
 if(!$_SESSION['logged_in']){
   header('Location: error.php');
   exit();
