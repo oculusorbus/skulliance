bf1051f3198d39127a9af16531a14cb98d0e1208
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-10-26 00:58:45 -0500

    Update skulliance.php

--- skulliance.php
+++ skulliance.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 if(!$_SESSION['logged_in']){
-	if(isset($_COOKIE['SessionCookie']))
+	if(isset($_COOKIE['SessionCookie'])){
 		$cookie = $_COOKIE['SessionCookie'];
 		$cookie = json_decode($cookie);
 		$_SESSION = $cookie;
