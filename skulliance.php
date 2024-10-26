de5dcff525021ad490352692754c62f18c0614c8
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-10-26 00:58:17 -0500

    Update skulliance.php

--- skulliance.php
+++ skulliance.php
@@ -3,16 +3,22 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-print_r($_SESSION);
-
 if(!$_SESSION['logged_in']){
-  header('Location: error.php');
-  exit();
+	if(isset($_COOKIE['SessionCookie']))
+		$cookie = $_COOKIE['SessionCookie'];
+		$cookie = json_decode($cookie);
+		$_SESSION = $cookie;
+	}else{
+  		header('Location: error.php');
+  		exit();
+	}
 }
 extract($_SESSION['userData']);
 //print_r($_SESSION['userData']);
 //print_r($_POST);
 //exit();
+$value = json_encode($_SESSION);
+setcookie("SessionCookie", $value, time()+3600);
 
 $avatar_url = "https://cdn.discordapp.com/avatars/$discord_id/$avatar.jpg";
 
