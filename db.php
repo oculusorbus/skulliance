61d7659cd251ed3d9809a5275a27c3af2efe7a14
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-10-27 02:10:01 -0500

    Update db.php

--- db.php
+++ db.php
@@ -935,7 +935,7 @@ function getCurrentMissions($conn){
 		echo "<td colspan='8' style='padding:0px;'>";
 		echo "<div class='w3-border'>";
 		$percentage = 100-((($days_remaining+($hours_remaining/24)+($minutes_remaining/1440)) / $row["duration"])*100);
-		echo "<div class='w3-grey' style='width:".$percentage."%'>".$time_message."</div>";
+		echo "<div class='w3-grey' style='width:".$percentage."%'></div>";
 		echo "</div>";
 		echo "</td>";
 		echo "</tr>";
