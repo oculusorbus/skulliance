3e25f98024d4479e0ba4a2e2dbca40dff556f88e
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-11-01 01:13:49 -0500

    Update skulliance.php

--- skulliance.php
+++ skulliance.php
@@ -490,7 +490,7 @@ function renderDailyRewardsSection(){
 	global $conn;
 	echo "<ul>";
 	echo "<li class='role no-border-style'><strong>Daily Random Rewards Streak</strong></li>";
-	echo "<li class='role'>Total Streaks Completed: ".getStreaksTotal($conn)."&nbsp;&nbsp;";
+	echo "<li class='role'>Total Streaks: ".getStreaksTotal($conn)."&nbsp;&nbsp;";
 	echo '<form style="position:relative;top:-4px;" action="leaderboards.php" method="post"><input type="hidden" name="filterbystreak" id="filterbystreak" value="monthly-streaks"><input type="submit" class="small-button" value="Leaderboard"></form>';
 	echo "</li>";
 	if(getDailyRewardEligibility($conn)) { 
