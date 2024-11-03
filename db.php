dad80b3643f1bcc1d0df430c19e9f912e11458fb
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-11-03 00:15:49 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1473,6 +1473,22 @@ function completeMission($conn, $mission_id, $quest_id){
 		  //echo "Error: " . $sql . "<br>" . $conn->error;
 		}
 		
+	  	// Delete Mission NFTs since they are unecessary after a mission is completed and take up a ton of table space which degrades query performance
+    	$sql = "DELETE FROM missions_nfts WHERE mission_id = '".$mission_id."'";
+    	if ($conn->query($sql) === TRUE) {
+    	  //echo "Record deleted successfully";
+    	} else {
+    	  //echo "Error: " . $sql . "<br>" . $conn->error;
+    	}
+	
+	  	// Delete Mission Consumables since they are unecessary after a mission is completed and take up a ton of table space which degrades query performance
+    	$sql = "DELETE FROM missions_consumables WHERE mission_id = '".$mission_id."'";
+    	if ($conn->query($sql) === TRUE) {
+    	  //echo "Record deleted successfully";
+    	} else {
+    	  //echo "Error: " . $sql . "<br>" . $conn->error;
+    	}
+		
 		$mission_result = array();
 		$project = getProjectInfo($conn, $project_id);
 		$mission_result["currency"] = $project["currency"];
