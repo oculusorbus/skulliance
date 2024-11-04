0cc3b01edcb38154f6ec08cb01b98da92529459e
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-11-04 15:17:04 -0600

    Update missions.php

--- missions.php
+++ missions.php
@@ -93,6 +93,7 @@ if(isset($_POST["start_all"])){
 	echo "<div class='main'>";
 	echo "<h2>Welcome to Skulliance</h2>";
 	echo "<p>Please connect a Cardano wallet to view missions.<br><br>Once you begin staking your NFTs, you will need to become a Skulliance member before you can claim items from the store.<br><br><a href='info.php'>View info on how to become a member of Skulliance.</a></p>";
+	echo "</div>";
 } 
 ?>
 <div id="quick-menu">
