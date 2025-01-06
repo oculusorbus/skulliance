9a0e9ac204ea41448114564906b100dc625ff4de
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-06 12:33:05 -0600

    Update upgrade-realm-location.php

--- ajax/upgrade-realm-location.php
+++ ajax/upgrade-realm-location.php
@@ -3,6 +3,7 @@ include '../db.php';
 include '../skulliance.php';
 
 if(isset($_GET['realm_id']) && isset($_GET['location_id']) && isset($_GET['duration']) && isset($_GET['cost'])){
+	// Need to double check duration and cost in case someone tries to manually override these variables in the JS function
 	upgradeRealmLocation($conn, $_GET['realm_id'], $_GET['location_id'], $_GET['duration'], $_GET['cost']);
 	$status = getRealmLocationUpgrade($conn, $_GET['realm_id'], $_GET['location_id']);
 	echo $status[$_GET['location_id']];
