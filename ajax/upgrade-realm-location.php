cd92c543723bbd0c2953f705b189d1cb5ae75936
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-06 11:40:57 -0600

    Update upgrade-realm-location.php

--- ajax/upgrade-realm-location.php
+++ ajax/upgrade-realm-location.php
@@ -4,7 +4,7 @@ include '../skulliance.php';
 
 if(isset($_GET['realm_id']) && isset($_GET['location_id']) && isset($_GET['duration'])){
 	upgradeRealmLocation($conn, $_GET['realm_id'], $_GET['location_id'], $_GET['duration']);
-	getRealmLocationUpgrade($conn, $_GET['realm_id'], $_GET['location_id']);
+	$status = getRealmLocationUpgrade($conn, $_GET['realm_id'], $_GET['location_id']);
 	echo $status[$_GET['location_id']];
 }else{
 	echo "No Get Variables";
