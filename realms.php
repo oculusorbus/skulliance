7e95d88ac07c761bb886be46bcb14e719c91e0e5
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-06 01:01:53 -0600

    Update realms.php

--- realms.php
+++ realms.php
@@ -17,11 +17,15 @@ if(isset($_SESSION['userData']['user_id'])){ ?>
 				
 			}else{
 				?>
+				<ul>
+				<li class="role">
 				<h2>Create Realm</h2>
 				<form action="realms.php" method="post">
 					<label for="realm">Realm Name</label><br>
 					<input type="text" id="realm" name="realm"><br>
 				</form>
+				</li>
+				</ul>
 				<?php
 			}
 			?>
