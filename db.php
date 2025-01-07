e70f69f66881655ce0116dc2594211556bb34a13
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-07 01:22:33 -0600

    Update db.php

--- db.php
+++ db.php
@@ -4233,7 +4233,7 @@ function deleteRealmLocationUpgrade($conn, $realm_id, $location_id){
 function getRealms($conn){
 	if(isset($_SESSION['userData']['user_id'])){
 		$origin_id = getRealmID($conn);
-		$sql = "SELECT DISTINCT realms.id AS realm_id, balance, SUM(balance) AS total_balance, realms.name AS realm_name, users.id AS user_id, users.username AS username, users.avatar AS avatar, users.discord_id AS discord_id
+		$sql = "SELECT DISTINCT realms.id AS realm_id, SUM(balance) AS total_balance, realms.name AS realm_name, users.id AS user_id, users.username AS username, users.avatar AS avatar, users.discord_id AS discord_id
 			    FROM realms INNER JOIN users ON users.id = realms.user_id INNER JOIN balances ON users.id = balances.user_id GROUP BY realm_id WITH ROLLUP";
 				/* WHERE users.id != '".$_SESSION['userData']['user_id']."' AND raids.origin_id != '".$origin_id."' AND raids.outcome != '0'"; */
 		$result = $conn->query($sql);
