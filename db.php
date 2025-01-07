30c7ed100763d99441b60b6ba58db4b78bd1fa48
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-06 23:18:52 -0600

    Update db.php

--- db.php
+++ db.php
@@ -4234,7 +4234,7 @@ function getRealms($conn){
 	if(isset($_SESSION['userData']['user_id'])){
 		$origin_id = getRealmID($conn);
 	$sql = "SELECT DISTINCT realms.id AS realm_id, realms.name AS realm_name, users.id AS user_id, users.username AS username, users.avatar AS avatar, users.discord_id AS discord_id, CASE WHEN outcome IS NULL THEN 'NONE' ELSE outcome END AS outcome 
-		    FROM realms INNER JOIN users ON users.id = realms.user_id LEFT JOIN raids ON raids.destination_id = realms.id GROUP BY realms.id";
+		    FROM realms INNER JOIN users ON users.id = realms.user_id LEFT JOIN raids ON raids.destination_id = realms.id WHERE raids.outcome = '0' GROUP BY realms.id";
 			/* WHERE users.id != '".$_SESSION['userData']['user_id']."' AND raids.origin_id != '".$origin_id."' AND raids.outcome != '0'"; */
 	$result = $conn->query($sql);
 	
