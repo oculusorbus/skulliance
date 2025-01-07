642e4f61e705796c933d51503d151bac94321b8e
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-06 20:00:54 -0600

    Update start_raid.php

--- ajax/start_raid.php
+++ ajax/start_raid.php
@@ -5,7 +5,7 @@ include '../skulliance.php';
 if(isset($_GET['destination_id']) && isset($_GET['duration'])){
 	// Need to double check duration in case someone tries to manually override these variables in the JS function
 	$realm_id = getRealmID($conn);
-	startRaid($conn, $realm_id, $_GET['destination_id'], $_GET['duration']);
+	startRaid($conn, $_GET['destination_id'], $_GET['duration']);
 }else{
 	echo "No Get Variables";
 }
