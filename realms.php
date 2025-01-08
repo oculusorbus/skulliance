46cd1505b68a7c569fd02ae80cc384ebafb2c1ac
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-07 23:31:40 -0600

    Update realms.php

--- realms.php
+++ realms.php
@@ -99,7 +99,7 @@ if(isset($_SESSION['userData']['user_id'])){ ?>
 		<div id="filter-nfts">
 			<label for="filterNFTs"><strong>Theme:</strong></label>
 			<select onchange="javascript:filterNFTs(this.options[this.selectedIndex].value);" name="filterNFTs" id="filterNFTs">';
-				$core_projects = getProjects($conn, "core");
+				$core_projects = array_reverse(getProjects($conn, "core"));
 				foreach($projects AS $id => $project){
 					echo '<option value="'.$id.'">'.$project["name"].'</option>';
 				}
