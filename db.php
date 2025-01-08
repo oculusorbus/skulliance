4957ff5781e1bff1a7396ad6babbd5267b08a61a
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-08 15:53:48 -0600

    Update db.php

--- db.php
+++ db.php
@@ -4716,7 +4716,7 @@ function alterRealmLocationLevel($conn, $raid_id, $faction, $location_id, $amoun
 	// Get Realm ID from raid based on faction
 	// Use Realm ID to update altered location level
 }
-
+/*
 function selectRandomProjectID($conn, $realm_id){
 	$sql = "SELECT project_id, balance FROM realms INNER JOIN users ON users.id = realms.user_id INNER JOIN balances ON balances.user_id = users.id WHERE id = '".$realm_id."'";
 	$result = $conn->query($sql);
@@ -4734,7 +4734,7 @@ function selectRandomProjectID($conn, $realm_id){
 	$selected_project[$random_project_id] = $project_ids[$random_project_id];
 	return $selected_project;
 }
-
+*/
 function assignRealmProjectRewards($conn, $raid_id, $project_id, $amount){
 	$sql = "INSERT INTO raids_projects (raid_id, project_id, amount)
 	VALUES ('".$raid_id."', '".$project_id."', '".$amount."')";
