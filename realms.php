d04cfc96bff17e8e9d559a992c67da019dfc83dc
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-07 23:10:48 -0600

    Update realms.php

--- realms.php
+++ realms.php
@@ -86,7 +86,7 @@ if(isset($_SESSION['userData']['user_id'])){ ?>
 	  </div>
 	  <div class="main">
 		<div id="realm">
-		<a id="realm" name="realm"></a>
+		<a id="#realm" name="#realm"></a>
 		<h2><?php echo checkRealm($conn)?getRealmName($conn):"Realm"; ?></h2>
 	    <div class="content realm" id="filtered-content">
 		<?php
