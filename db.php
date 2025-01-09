2e58e90f917407c898a8ec38800241a56cc29570
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-09 11:08:17 -0600

    Update db.php

--- db.php
+++ db.php
@@ -4325,12 +4325,12 @@ function getRealms($conn){
 				echo "<td width='25%' valign='top' align='left'>";
 				echo "<table id='transactions' style='border-style:none'>";
 				echo "<tr>";
-				echo "<td align='right' width='20%'>";
+				echo "<td align='right' width='10%'>";
 					if($row["avatar"] != ""){
 						echo "<img style='width:50px' onError='this.src=\"/staking/icons/skull.png\";' src='https://cdn.discordapp.com/avatars/".$row["discord_id"]."/".$row["avatar"].".jpg' class='icon'/>";
 					}
 				echo "</td>";
-				echo "<td width='80%'>".$row["username"]."</td>";
+				echo "<td width='90%'>".$row["username"]."</td>";
 				echo "</tr>";
 				echo "</tr>";
 				echo "<tr>";
