1173103308595d48fe5edaee7260bea76f3fef1b
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-08 20:53:13 -0600

    Update db.php

--- db.php
+++ db.php
@@ -4504,8 +4504,6 @@ function getRaids($conn, $type){
 			echo "<table id='transactions'>";
 			echo "<th width='6%'>Icon</th><th width='18%' align='left'>Realm</th><th width='6%'>Avatar</th><th width='22%' align='left'>Username</th><th width='12%' align='left'>Time Left</th><th width='12%' align='left'>Status</th></th><th width='12%' align='left'>".$results1." Results</th></th><th width='12%' align='left'>".$results2." Results</th>";
 			$rows = array();
-			$progress_rows = array();
-			$completed_rows = array();
 			while($row = $result->fetch_assoc()) {
 				$date = strtotime('+'.$row["duration"].' day', strtotime($row["created_date"]));
 				$remaining = $date - time();
@@ -4542,13 +4540,9 @@ function getRaids($conn, $type){
 					}
 				}
 				if($status == "Completed"){
-					$decimal = $days_remaining.".".(($hours_remaining<10)?"0".$hours_remaining:$hours_remaining).(($minutes_remaining<10)?"0".$minutes_remaining:$minutes_remaining).$row["raid_id"];
-					$completed_rows = array_merge($rows, $completed_rows);
-					unset($rows);
+					$decimal = $row["raid_id"];
 				}else{
 					$decimal = $days_remaining.".".(($hours_remaining<10)?"0".$hours_remaining:$hours_remaining).(($minutes_remaining<10)?"0".$minutes_remaining:$minutes_remaining).$row["raid_id"];
-					$progress_rows = array_merge($rows, $progress_rows);
-					unset($rows);
 				}
 				$rows[$decimal] = "";
 				$rows[$decimal] .= "<tr>";
@@ -4590,14 +4584,9 @@ function getRaids($conn, $type){
 				$rows[$decimal] .= "</td>";
 				$rows[$decimal] .= "</tr>";
 			}
-			ksort($progress_rows);
-			foreach($progress_rows AS $progress_duration => $progress_output){
-			  echo $progress_output;
-			}
-			ksort($completed_rows);
-			$completed_rows = array_reverse($completed_rows);
-			foreach($completed_rows AS $completed_duration => $completed_output){
-			  echo $completed_output;
+			ksort($rows);
+			foreach($rows AS $duration => $output){
+			  echo $output;
 			}
 			echo "</table>";
 		} else {
