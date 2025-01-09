36e1839c36df31d486b4b3c835396dc1b847dac5
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-09 00:56:38 -0600

    Update db.php

--- db.php
+++ db.php
@@ -4325,16 +4325,12 @@ function getRealms($conn){
 				echo "<td width='25%' valign='top' align='left'>";
 				echo "<table id='transactions' style='border-style:none'>";
 				echo "<tr>";
-				echo "<td align='right'>";
-				if($row["avatar"] != ""){
-					echo "<img style='width:50px' onError='this.src=\"/staking/icons/skull.png\";' src='https://cdn.discordapp.com/avatars/".$row["discord_id"]."/".$row["avatar"].".jpg' class='icon'/>";
-				}
-				echo "</td>";
-				echo "<td>&nbsp;</td>";
-				echo "</tr>";
-				echo "<tr>";
 				echo "<td align='right' width='50%'>".$row["username"]."</td>";
-				echo "<td width='50%'>&nbsp;</td>";
+				echo "<td width='50%'>";
+					if($row["avatar"] != ""){
+						echo "<img style='width:50px' onError='this.src=\"/staking/icons/skull.png\";' src='https://cdn.discordapp.com/avatars/".$row["discord_id"]."/".$row["avatar"].".jpg' class='icon'/>";
+					}
+				echo "</td>";
 				echo "</tr>";
 				echo "</tr>";
 				echo "<tr>";
