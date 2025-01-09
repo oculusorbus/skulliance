5874793164e90cec4a97f6921daa0034ee00c0ae
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-08 20:22:37 -0600

    Update db.php

--- db.php
+++ db.php
@@ -4540,8 +4540,6 @@ function getRaids($conn, $type){
 					}
 				}
 				if($status == "Completed"){
-					$decimal = $row["raid_id"];
-				}else{
 					$decimal = $days_remaining.".".(($hours_remaining<10)?"0".$hours_remaining:$hours_remaining).(($minutes_remaining<10)?"0".$minutes_remaining:$minutes_remaining).$row["raid_id"];
 				}
 				$rows[$decimal] = "";
