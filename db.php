db698ceffd4a41683974dcfdb0a418ae9a3c8e5c
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-09 13:00:49 -0600

    Update db.php

--- db.php
+++ db.php
@@ -4312,7 +4312,7 @@ function getRealms($conn){
 		echo "<table width='100%' id='transactions'>";
 		if ($result->num_rows > 0) {
 			while($row = $result->fetch_assoc()) {
-				echo "<th>".$row['realm_name']."</th><th>Raid Details</th><th>Location Levels</th><th>Top Points Balances</th>";
+				echo "<th>".ucfirst($row['realm_name'])."</th><th>Raid Details</th><th>Location Levels</th><th>Top Points Balances</th>";
 				echo "<tr>";
 				echo "<td width='25%' valign='top' align='left'>";
 				echo "<table id='transactions' style='border-style:none'>";
