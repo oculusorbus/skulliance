020d2709d4d3950c0a94ca0f9a3fd6ae614042df
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-11 02:04:22 -0600

    Update skulliance.js

--- skulliance.js
+++ skulliance.js
@@ -449,6 +449,7 @@ function toggleRaids(arrow, category, results){
 	    if (xhttp.status == 200) {
 			// Access the data returned by the server
 			var data = xhttp.responseText;
+			console.log(data);
 	    } else {
 	      // Handle error
 			alert("AJAX Error");
