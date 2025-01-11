9e99a1919e83de0c39b5722673488df9c4f0be73
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-10 23:37:19 -0600

    Update db.php

--- db.php
+++ db.php
@@ -4895,7 +4895,7 @@ function getRaids($conn, $type, $status="pending"){
 function getTotalRaids($conn){
 	$month_sql = "SELECT (SELECT COUNT(success_raids.id) FROM raids AS success_raids INNER JOIN realms AS success_realms ON success_realms.id = success_raids.offense_id INNER JOIN users AS success_users ON success_users.id = success_realms.user_id 
 				  WHERE success_raids.outcome = '1' AND success_users.id = users.id AND DATE(success_raids.created_date) >= DATE_FORMAT(CURDATE(),'%Y-%m-01')) AS success, 
-	             (SELECT COUNT(failed_raids.id) FROM raids AS failed_raids INNER JOIN realms AS  failed_realms ON failed_realms.id = failed_raids.offense_id INNER JOIN users AS failed_user ON failed_users.id = failed_realms.user_id 
+	             (SELECT COUNT(failed_raids.id) FROM raids AS failed_raids INNER JOIN realms AS  failed_realms ON failed_realms.id = failed_raids.offense_id INNER JOIN users AS failed_users ON failed_users.id = failed_realms.user_id 
 				  WHERE failed_raids.outcome = '2' AND failed_users.id = users.id AND DATE(failed_raids.created_date) >= DATE_FORMAT(CURDATE(),'%Y-%m-01')) AS failure, 
 				 (SELECT COUNT(progress_raids.id) FROM raids AS progress_raids INNER JOIN realms AS  progress_realms ON progress_realms.id = progress_raids.offense_id INNER JOIN users AS progress_users ON progress_users.id = progress_realms.user_id 
 				  WHERE progress_raids.outcome = '0' AND progress_users.id = users.id AND DATE(progress_raids.created_date) >= DATE_FORMAT(CURDATE(),'%Y-%m-01')) AS progress, 
