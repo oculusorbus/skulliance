676b212b12d84ba6105a20be998489aa8a4a45dd
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-10 20:32:00 -0600

    Update upgrade-realm-location.php

--- ajax/upgrade-realm-location.php
+++ ajax/upgrade-realm-location.php
@@ -4,7 +4,7 @@ include '../skulliance.php';
 
 if(isset($_GET['realm_id']) && isset($_GET['location_id']) && isset($_GET['duration']) && isset($_GET['cost'])){
 	// Need to double check duration and cost in case someone tries to manually override these variables in the JS function
-	if(checkRealmLocationUpgrade($conn, $_GET['realm_id', $_GET['location_id'])){
+	if(checkRealmLocationUpgrade($conn, $_GET['realm_id'], $_GET['location_id'])){
 		echo "Upgrade Already in Progress<br>";
 	}else{
 		upgradeRealmLocation($conn, $_GET['realm_id'], $_GET['location_id'], $_GET['duration'], $_GET['cost']);
