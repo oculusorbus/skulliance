9f6918f0abd71a52a80584b960c06f61522fdc1c
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-11 02:05:38 -0600

    Update toggle-raids.php

--- ajax/toggle-raids.php
+++ ajax/toggle-raids.php
@@ -4,6 +4,7 @@ include '../skulliance.php';
 
 if(isset($_GET['visibility']) && isset($_GET['type']) && isset($_GET['status'])){
 	$_SESSION['userData'][$_GET['type'].'-'.$_GET['status'].'-raids'] = $_GET['visibility'];
+	echo $_GET['type'].'-'.$_GET['status'].'-raids';
 }else{
 	echo "No Session";
 }
