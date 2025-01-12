c4c8b6a4a507578cf2e6d25577489656207ad5c5
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-12 01:13:50 -0600

    Realms

--- webhooks.php
+++ webhooks.php
@@ -17,6 +17,7 @@ include('credentials/webhooks_credentials.php');
 		}else if($channel == "innercircle"){
 			$webhook = getInnerCircleWebhook();
 		}else if($channel == "realms"){
+			alert('realms');
 			$webhook = getRealmsWebhook();
 		}else if($channel == "raids"){
 			$webhook = getRaidsWebhook();
