6704be970840054cf7cdbd873ba4ad45b9d1a4d0
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-12 02:15:43 -0600

    Update realms.php

--- realms.php
+++ realms.php
@@ -6,8 +6,6 @@ include 'verify.php';
 include 'skulliance.php';
 include 'header.php';
 
-print_r($_SESSION['userData']);
-
 if(isset($_POST['realm'])){
 	if(!checkRealm($conn)){
 		createRealm($conn, $_POST['realm']);
