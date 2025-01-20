7c4d4e8a9f179db433ca80afd1887010dc841b54
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-19 21:44:26 -0600

    Update realms.php

--- realms.php
+++ realms.php
@@ -123,7 +123,7 @@ if(isset($_SESSION['userData']['user_id'])){
 					if($activation == "true"){
 						echo '<input class="button" type="button" value="Reactivate Realm" onclick="reactivateRealm('.$realm_id.');">';
 					}else{
-						echo '<p>Your Realm cannot be reactivated until '.date('F d, Y', strtotime($activation)).'</p>';
+						echo '<p>Your Realm cannot be reactivated until '.date('F j, Y', strtotime($activation)).'</p>';
 					}
 				}
 			}else{
