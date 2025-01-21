d2b6ee50b34b9dd99d8d3d3e0c616e6717d684d7
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-20 18:25:51 -0600

    Update get-quests.php

--- ajax/get-quests.php
+++ ajax/get-quests.php
@@ -2,7 +2,7 @@
 include '../db.php';
 include '../skulliance.php';
 
-if(isset($_GET['project_id']){
+if(isset($_GET['project_id'])){
 	getMissions($conn, $quest_id=0, $_GET['project_id']);
 }else{
 	echo "No GET variables";
