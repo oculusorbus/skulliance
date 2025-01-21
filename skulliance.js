8cb300538067178b2c8ca3271cc30f8e797ecc32
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-21 03:19:57 -0600

    Update skulliance.js

--- skulliance.js
+++ skulliance.js
@@ -419,10 +419,7 @@ function toggleCurrentMissions(arrow){
 			document.getElementById('current-missions-container').innerHTML = data;
 			if(data != ""){
 				document.getElementById('current-down').src = 'icons/down.png';
-				var claimButton = document.getElementById('claim-missions-button');
-				if (typeof(claimButton) != 'undefined' && claimButton != null){
-					document.getElementById('current-missions-container').insertBefore(document.getElementById('claim-missions-button'), document.getElementById('current-missions-container').firstChild);
-				}
+				document.getElementById('current-missions-container').insertBefore(document.getElementById('claim-missions-button'), document.getElementById('current-missions-container').firstChild);
 			}
 	    } else {
 	      // Handle error
