a41e57d68e19d20f4f83788a38f8e75542475b51
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-21 20:30:03 -0600

    Update discord-tasks.php

--- discord-tasks.php
+++ discord-tasks.php
@@ -3,9 +3,9 @@ include '../db.php';
 
 if(isset($_GET['discord_id'])){
 	if(isset($_GET['message'])){
-		updateDiscordMessageStatus($conn, $_GET['discord_id'], $status)
+		updateDiscordMessageStatus($conn, $_GET['discord_id'], 1);
 	}else if(isset($_GET['reaction']){
-		updateDiscordReactionStatus($conn, $_GET['discord_id'], $status)
+		updateDiscordReactionStatus($conn, $_GET['discord_id'], 1);
 	}
 }
 
