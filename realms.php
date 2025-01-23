db97b5c59aea278b84f69fbedca9d12b4c76f008
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-23 02:12:02 -0600

    Update realms.php

--- realms.php
+++ realms.php
@@ -96,11 +96,11 @@ if(isset($_SESSION['userData']['user_id'])){ ?>
 											<?php
 												}else{
 													echo "<span id='upgrade-message-".$location_id."'>Need ".number_format($cost-$balance)." ".$projects[$location_id]['currency']."</span>";
-												}
 												?>
 												<br><br>
 												<input id="points-button-<?php echo $location_id; ?>" class="small-button" type="button" value="<?php echo $points_multiplier; ?>x Points Upgrade" onclick="togglePointsButtons('disable');pointsOption(this, <?php echo $realm_id;?>, <?php echo $location_id;?>, <?php echo $duration;?>, <?php echo $cost; ?>)">
 												<?php
+												}
 										    }else{ 
 												echo $status[$location_id];
 											}
