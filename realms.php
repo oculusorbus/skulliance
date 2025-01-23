179352d5223c9e5e8daac1821d8aa1c487c47db4
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-23 03:28:40 -0600

    Update realms.php

--- realms.php
+++ realms.php
@@ -142,9 +142,9 @@ if(isset($_SESSION['userData']['user_id'])){ ?>
 					<label for="disclaimer">Disclaimer</label><br>
 					<p id="disclaimer">By creating your realm, you agree to being vulnerable to raids from other realm owners who may attack your realm, damage your locations, and steal some of your points. You can raid other realms but raid failures also result in damage to your locations. If you anticipate that you are not going to be dedicated to protecting your realm and upgrading/raiding on a regular basis, don't feel obligated to create a realm. Abandoned realms are a prime target for plundering which can lead to complete poverty and devastation.
 <br><br>
-Realms require core project points as well as DIAMOND to upgrade offense, defense, and transport locations. The more core projects points you have, the stronger your realm will be. Without being able to upgrade all your locations, your ability to attack, defend, and travel may be impeded.
+Realms require core project points as well as DIAMOND to upgrade offense, defense, and transport locations. The more core projects points you have, the stronger your realm will be. Without being able to upgrade all your locations, your ability to attack, defend, and travel may be impeded. With that being said, you can now upgrade locations using other points, but it will be costlier than using core project points.
 <br><br>
-With that being said, Skulliance is offering a promotional incentive to participate in realms. Stakers establishing new realms will receive the following starter pack of core project points:
+Skulliance is offering a promotional incentive to participate in realms. Stakers establishing new realms will receive the following starter pack of core project points:
 <ul>
 	<li>1K STAR</li>
 	<li>1K DREAD</li>
@@ -155,7 +155,7 @@ With that being said, Skulliance is offering a promotional incentive to particip
 	<li>1K DIAMOND</li>
 </ul>
 <br>
-This allocation of core project points should provide you the opportunity to upgrade your locations to level 4. You can choose to deactivate your realm at any time and no longer participate in raids. But you will have to wait a month before being allowed to reactivate your realm. If you have not raided in over a month, your realm is subject to being automatically deactivated. You can reactivate an automatically deactivated realm at any time with no penalty.</p>
+This allocation of core project points should provide you the opportunity to upgrade your locations to level 4 before having to rely on other points at a higher cost. You can choose to deactivate your realm at any time and no longer participate in raids. But you will have to wait a month before being allowed to reactivate your realm. If you have not raided in over a month, your realm is subject to being automatically deactivated. You can reactivate an automatically deactivated realm at any time with no penalty.</p>
 				</form>
 				</li>
 				</ul>
