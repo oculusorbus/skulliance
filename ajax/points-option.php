874a00576abece0874066eb36f4cebb7c8720a6a
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-22 23:10:39 -0600

    Update points-option.php

--- ajax/points-option.php
+++ ajax/points-option.php
@@ -5,7 +5,7 @@ include '../skulliance.php';
 if(isset($_GET['realm_id']) && isset($_GET['location_id']) && isset($_GET['duration']) && isset($_GET['cost'])){
 	// Need to double check duration and cost in case someone tries to manually override these variables in the JS function
 	$balances = getLocationBalances($conn, $_SESSION['userData']['user_id']);
-	echo '<select name="points" id="points-'.$_GET['location_id'].'" onchange="document.getElementById(\'points-button-'.$_GET['location_id'].'\').value=\'Upgrade '.number_format($points_multiplier*$_GET['cost']).' \'+this.options[this.selectedIndex].name;">';
+	echo '<select name="points" id="points-'.$_GET['location_id'].'" onchange="document.getElementById(\'points-button-'.$_GET['location_id'].'\').value=\'Upgrade '.number_format($points_multiplier*$_GET['cost']).' \'+this.options[this.selectedIndex].value;">';
 	 echo '<option name="Points" value="0">Select Points Balance</option>';
 	foreach($balances AS $project_id => $balance){
 		if($balance["balance"] >= ($points_multiplier*$_GET['cost'])){
