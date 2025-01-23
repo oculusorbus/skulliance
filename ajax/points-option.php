6a0b6bea13da9533130c1d854266f4b5f01000cf
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-22 22:54:47 -0600

    Update points-option.php

--- ajax/points-option.php
+++ ajax/points-option.php
@@ -6,7 +6,7 @@ if(isset($_GET['realm_id']) && isset($_GET['location_id']) && isset($_GET['durat
 	// Need to double check duration and cost in case someone tries to manually override these variables in the JS function
 	$balances = getLocationBalances($conn, $_SESSION['userData']['user_id']);
 	echo '<select name="points" id="points-'.$_GET['location_id'].'" onchange="document.getElementById(\'points-button-'.$_GET['location_id'].'\').value=\'Upgrade '.number_format($points_multiplier*$_GET['cost']).' \'+this.value;">';
-	 echo '<option value="Points">Select Points Balance</option>';
+	 echo '<option value="0">Select Points Balance</option>';
 	foreach($balances AS $project_id => $balance){
 		if($balance["balance"] >= ($points_multiplier*$_GET['cost'])){
 		  echo '<option value="'.$project_id.'">'.$balance["currency"].' ('.number_format($balance["balance"]).')</option>';
