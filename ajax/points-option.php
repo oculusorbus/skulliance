aaf5b7fdd38d9ee72965c90bb8372cea8122d111
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-23 01:16:38 -0600

    Update points-option.php

--- ajax/points-option.php
+++ ajax/points-option.php
@@ -5,8 +5,8 @@ include '../skulliance.php';
 if(isset($_GET['realm_id']) && isset($_GET['location_id']) && isset($_GET['duration']) && isset($_GET['cost'])){
 	// Need to double check duration and cost in case someone tries to manually override these variables in the JS function
 	$balances = getLocationBalances($conn, $_SESSION['userData']['user_id']);
-	echo '<span class="dropdown" id="points-section-'.$_GET['location_id'].'">';
-	echo '<select name="points" id="points-'.$_GET['location_id'].'" onchange="document.getElementById(\'points-button-'.$_GET['location_id'].'\').value=\'Upgrade '.number_format($points_multiplier*$_GET['cost']).' \'+this.options[this.selectedIndex].id;">';
+	echo '<span id="points-section-'.$_GET['location_id'].'">';
+	echo '<select class="dropdown" name="points" id="points-'.$_GET['location_id'].'" onchange="document.getElementById(\'points-button-'.$_GET['location_id'].'\').value=\'Upgrade '.number_format($points_multiplier*$_GET['cost']).' \'+this.options[this.selectedIndex].id;">';
 	echo '<option id="Points" name="Points" value="0">Select Points Balance</option>';
 	foreach($balances AS $project_id => $balance){
 		if($balance["balance"] >= ($points_multiplier*$_GET['cost']) && $project_id != $_GET['location_id']){
