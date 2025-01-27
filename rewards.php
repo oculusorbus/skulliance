2258b8eed0fcd295af52faa8cba05f0d535ec5d9
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-27 17:04:26 -0600

    Update rewards.php

--- rewards.php
+++ rewards.php
@@ -5,6 +5,20 @@ include 'webhooks.php';
 if(isset($argv)){
 parse_str(implode('&', array_slice($argv, 1)), $_GET);
 }
+
+if(isset($_GET['rewards'])){
+	set_time_limit(0);
+	// Get project percentages for Diamond Skull delegations
+	$percentages = array();
+	$percentages = getProjectDelegationPercentages($conn);
+	// Determine whether Diamond Skulls should get a bonus
+	$diamond_skull_bonus = getDiamondSkullBonus($percentages);
+	// Deploy rewards for all users of the platform
+	updateBalances($conn, $diamond_skull_bonus);
+	// Deploy rewards for Diamond Skull delegation
+	deployDiamondSkullRewards($conn, $percentages);
+}
+
 if(isset($_GET['missions'])){
 	checkMissionsLeaderboard($conn, false, true);
 }
