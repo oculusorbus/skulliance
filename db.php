76dd4b9fc3e2ddcfd47ec413db93ce38d74b8329
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-01-31 03:22:05 -0600

    Update db.php

--- db.php
+++ db.php
@@ -5609,8 +5609,8 @@ function getTotalFactionRaids($conn){
 	WHERE 
 	DATE(raids.created_date) >= DATE_FORMAT(CURDATE(),'%Y-%m-01' 
 	AND 
-	realms.project_id = '".$project_id."' 
-    GROUP BY 
+	realms.project_id = '".$project_id."' ".
+    "GROUP BY 
     realms.project_id
 	ORDER BY 
     total DESC;";
@@ -5725,8 +5725,8 @@ function getTotalFactionRaids($conn){
 	    INNER JOIN projects ON projects.id = realms.project_id 
 	    INNER JOIN raids ON raids.offense_id = realms.id 
 		WHERE 
-		realms.project_id = '".$project_id."' 
-	    GROUP BY 
+		realms.project_id = '".$project_id."' ".
+	    "GROUP BY 
 	    realms.project_id
 		ORDER BY 
 	    total DESC;"
