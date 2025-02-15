7d90e4e8aca776a7147f8068cf64b8e2e2356c0c
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-02-15 16:13:49 -0600

    Update db.php

--- db.php
+++ db.php
@@ -4898,7 +4898,13 @@ function getRealms($conn, $sort){
 				if($sort == "random"){
 					$key = $row['realm_id'];
 				}else if($sort == "weakness" || $sort == "strength"){
-					$key = ($raw_defense+$raw_defense_offense).".".$row['realm_id'];
+					$calulation = 0;
+					if($sort == "weakness"){
+						$calculation = $raw_defense+$raw_defense_offense;
+					}else if($sort == "strength"){
+						$calculation = $raw_defense;
+					}
+					$key = $calculation.".".$row['realm_id'];
 				}else if($sort == "wealth"){
 					$key = array_sum($balances).".".$row['realm_id'];
 				}
