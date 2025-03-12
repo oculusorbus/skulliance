07113b8ee759f88e5a2c3bffeafd18bd5e7e78be
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-03-11 19:22:42 -0500

    Update db.php

--- db.php
+++ db.php
@@ -5111,16 +5111,15 @@ function getRealms($conn, $sort, $group){
 						$raiding = true;
 						$value = "GET REVENGE";
 					}
-					echo $raiding;
 					// Prevents established realms from rading new realms, but allows for new realms to raid each other.
-					if(($raw_defense == 0 && $raw_offense != 0) && $raiding == false){
+					if(($raw_defense == 0 && $raw_offense != 0) && !$raiding){
 						$output[$key] .= "<strong>Establishing Realm</strong><br><br>";
 						$unset = true;
-					}else if((($offense-$defense) > 3) && $raiding == false){
+					}else if((($offense-$defense) > 3) && !$raiding){
 						$level_range = (($offense-$defense)-3);
 						$output[$key] .= "<strong>".$level_range." ".(($level_range == 1)?"Level":"Levels")." Out of Range</strong><br><br>";
 						$unset = true;
-					}else if(!in_array($row['realm_id'], getRecentRaidedRealms($conn)) || $raiding == true){
+					}else if(!in_array($row['realm_id'], getRecentRaidedRealms($conn)) || $raiding){
 						if(checkMaxRaids($conn, $offense_id)){
 							$output[$key] .= "<input type='button' class='raid-button' value='".$value."' onclick='startRaid(this, ".$row['realm_id'].", ".$duration.");'><br><br>";
 						}else{
