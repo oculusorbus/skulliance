a9624ad2d5d64a446992e3d3f93076bdba0d4e90
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-04-08 22:39:43 -0500

    Update monstrocity.php

--- monstrocity.php
+++ monstrocity.php
@@ -714,6 +714,7 @@
 	      <option value="monstrocity">Monstrocity</option>
 		  <option value="muses">Muses of the Multiverse</option>
 	      <option value="darkula">Darkula - Island of Uncanny Neighbors</option>
+		  <option value="deadpophell">Dead Pop Hell (NSFW)</option>
 	    </select>
 	  </div>
 	  <p><a href="https://www.jpg.store/collection/monstrocity" target="_blank">Purchase Monstrocity NFTs</a> to Add More Characters</p>
