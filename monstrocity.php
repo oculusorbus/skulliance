566c23e0a71f26d13c7083472aec70503b164091
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-04-10 17:30:09 -0500

    Update monstrocity.php

--- monstrocity.php
+++ monstrocity.php
@@ -850,7 +850,7 @@
 	      <option value="darkula">Darkula - Island of Uncanny Neighbors</option>
 		  <option value="deadpophell">Dead Pop Hell - NSFW</option>
   		  <option value="discosolaris">Disco Solaris - Moebius Pioneers</option>
-		  <option value="discosolaris">Havoc Worlds - Season 1</option>
+		  <option value="havocworlds">Havoc Worlds - Season 1</option>
 		  <option value="maxi">Maxingo - Digital Hell Citizens 2: Fighters</option>
 		  <option value="muses">Muses of the Multiverse - Season 1</option>
 		  <option value="omen">Nemonium - Omen Legends</option>
