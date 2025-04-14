dba8d74cdf8c78328903de6c464b23c23a9d9646
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-04-14 02:13:41 -0500

    Update monstrocity-test.php

--- monstrocity-test.php
+++ monstrocity-test.php
@@ -78,12 +78,9 @@ if(isset($_SESSION)){
 	}
 
 	.game-container {
-	  margin-top: 20px;
-	  margin-bottom: 20px;
 	  text-align: center;
 	  padding: 20px;
 	  background-color: #002f44;
-	  border-radius: 10px;
 	  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
 	  width: 100%;
 	  height: 100%;
@@ -274,7 +271,7 @@ if(isset($_SESSION)){
 
 	#game-over-container {
 	    position: absolute;
-	    top: 450px;
+	    top: 430px;
 	    left: 50%;
 	    width: 260px;
 	    max-width: 260px;
