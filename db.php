857fc5f968008bb3659714a0bf609265ee29480e
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-04-24 15:13:03 -0500

    Update db.php

--- db.php
+++ db.php
@@ -5761,7 +5761,7 @@ function getRaids($conn, $type, $status="pending", $history=false){
 			    $final_output .= $output;
 			}
 			$final_output .= "</table>";
-			if(!$history){
+			if(!$history && $status != "pending"){
 				$final_output .= "<a href='raids.php'>View ".ucfirst($type)." Raids History</a>";
 			}
 			$final_output .= "</div>";
