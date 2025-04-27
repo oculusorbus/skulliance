9c52a5e981cc56aeb36aa02538b3700e38d74694
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2025-04-27 02:38:08 -0500

    Update monstrocity.php

--- monstrocity.php
+++ monstrocity.php
@@ -1300,16 +1300,6 @@ if (isset($_SESSION['userData']) && is_array($_SESSION['userData'])) {
 			  extension: "png" // Applies only to character images
 	        },
 	        {
-	          value: "animeorigins",
-	          project: "Nel",
-	          title: "Anime Origins",
-	          policyIds: "",
-	          orientations: "",
-	          ipfsPrefixes: "",
-			  background: true,
-			  extension: "png" // Applies only to character images
-	        },
-	        {
 	          value: "cardanians",
 	          project: "Cardanians",
 	          title: "Cardanian Snow Globes (GIF)",
@@ -1333,6 +1323,16 @@ if (isset($_SESSION['userData']) && is_array($_SESSION['userData'])) {
 	          value: "handies",
 	          project: "Handies",
 	          title: "Handies",
+	          policyIds: "",
+	          orientations: "",
+	          ipfsPrefixes: "",
+			  background: true,
+			  extension: "png" // Applies only to character images
+	        },
+	        {
+	          value: "animeorigins",
+	          project: "Nel",
+	          title: "Anime Origins",
 	          policyIds: "",
 	          orientations: "",
 	          ipfsPrefixes: "",
