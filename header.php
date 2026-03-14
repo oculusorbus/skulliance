ecdae66c04ff6f64c9889e8533d4ceb8a6f54c8e
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2026-03-13 21:44:53 -0500

    Navigation Overhaul

--- header.php
+++ header.php
@@ -20,16 +20,21 @@
 	  // Toggle burger menu
 	  function toggleMenu(){
 	  	if(document.getElementById('burger-icon').src == "https://www.skulliance.io/staking/images/menu.png"){
-	  	  	//document.getElementById('navbar').style.display='flex';
 	  		document.getElementById('burger-icon').src = "https://www.skulliance.io/staking/images/close.png";
 	  		document.getElementById("navbar").classList.add('show-menu');
 	  		document.getElementById("navbar").classList.remove('hide-menu');
 	  	}else{
-	  	  	//document.getElementById('navbar').style.display='none';
 	  		document.getElementById('burger-icon').src = "https://www.skulliance.io/staking/images/menu.png";
 	  		document.getElementById("navbar").classList.add('hide-menu');
 	  		document.getElementById("navbar").classList.remove('show-menu');
 	  	}
+	  }
+	  // Toggle dropdown (mobile)
+	  function toggleDropdown(el){
+	  	var menu = el.nextElementSibling;
+	  	var isOpen = menu.classList.contains('open');
+	  	document.querySelectorAll('.nav-dropdown-menu.open').forEach(function(m){ m.classList.remove('open'); });
+	  	if(!isOpen) menu.classList.add('open');
 	  }
   </script>
   <?php if(isset($extra_head)) echo $extra_head; ?>
@@ -46,19 +51,42 @@
 	      <img class="rounded-full" src="<?php echo $avatar_url?>" />
 		  <?php } ?>
 		  <a href="profile.php<?php echo (isset($name)) ? '?username='.urlencode($name) : ''; ?>"><?php echo (isset($name))?$name:"";?></a>
-		  <a class="navbar-first" href="dashboard.php">Dashboard</a>
-  		  <a href="missions.php">Missions</a>
-		  <a href="realms.php">Realms</a>
-   		  <a href="monstrocity.php" target="_blank">Match 3 RPG</a>
-  		  <a href="skullswap.php">Skull Swap</a>
-		  <a href="store.php">Store</a>
+
+		  <!-- Play -->
+		  <div class="nav-dropdown navbar-first">
+		    <span class="nav-dropdown-trigger" onclick="toggleDropdown(this)">Play &#9660;</span>
+		    <div class="nav-dropdown-menu">
+		      <a href="missions.php">Missions</a>
+		      <a href="realms.php">Realms</a>
+		      <a href="monstrocity.php" target="_blank">Match 3 RPG</a>
+		      <a href="skullswap.php">Skull Swap</a>
+		      <a href="https://www.madballs.net/drop-ship" target="_blank">Drop Ship</a>
+		    </div>
+		  </div>
+
+		  <!-- NFTs -->
+		  <div class="nav-dropdown">
+		    <span class="nav-dropdown-trigger" onclick="toggleDropdown(this)">NFTs &#9660;</span>
+		    <div class="nav-dropdown-menu">
+		      <a href="dashboard.php">Dashboard</a>
+		      <a href="store.php">Store</a>
+		      <a href="collections.php">Collections</a>
+		      <a href="diamond-skulls.php">Diamond Skulls</a>
+		      <a href="skulliverse.php">Skulliverse</a>
+		    </div>
+		  </div>
+
 		  <a href="leaderboards.php">Leaderboards</a>
-		  <a href="collections.php">Collections</a>
-		  <a href="transactions.php">Transactions</a>
-  		  <a href="diamond-skulls.php">Diamond Skulls</a>
-		  <a href="skulliverse.php">Skulliverse</a>
-		  <a href="https://www.madballs.net/drop-ship" target="_blank">Drop Ship</a>
-    	  <a href="wallets.php">Wallets</a>
+
+		  <!-- Account -->
+		  <div class="nav-dropdown">
+		    <span class="nav-dropdown-trigger" onclick="toggleDropdown(this)">Account &#9660;</span>
+		    <div class="nav-dropdown-menu">
+		      <a href="transactions.php">Transactions</a>
+		      <a href="wallets.php">Wallets</a>
+		    </div>
+		  </div>
+
 		  <a href="logout.php">Logout</a>
 		  <?php } ?>
 		</div>
